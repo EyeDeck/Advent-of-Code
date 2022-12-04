ebd89bad23f9d3f5860d5e7cfe72375038860eb5
EyeDeck <eyedeck@gmail.com>
2022-12-04 01:07:53 -0500

    Wash 2022 day 4
    Proper solution using logic instead of wasteful set() nonsense.
    
    Also, I worked out why the sorting "shortcut" didn't work as intended before, and it was because, with the input parsed like this:
    2-4,6-8 = [[2,4],[6-8]]
    I not only had to sort by the first item in each list, but also first reverse-sort by the second item in the list in case the ranges start on the same number.

--- AoC2022/d4.py
+++ AoC2022/d4.py
@@ -1,24 +1,19 @@
-from aoc import *
+import sys
+from operator import itemgetter
 
 
-def p1():
-    total = 0
+def solve():
+    p1, p2 = 0, 0
     for line in data:
+        line = sorted(line, key=itemgetter(1), reverse=True)
         line = sorted(line, key=itemgetter(0))
-        # print(line)
-        # if line[0][0] <= line[1][0] and line[0][1] >= line[1][1]:
-        if (line[0][0] <= line[1][0] and line[0][1] >= line[1][1]) or (line[1][0] <= line[0][0] and line[1][1] >= line[0][1]):
-            print(line)
-            total += 1
-    return total
 
+        if line[0][1] >= line[1][1]:
+            p1 += 1
 
-def p2():
-    total = 0
-    for line in data:
-        if len( set(i for i in range(line[0][0], line[0][1]+1)) & set(i for i in range(line[1][0], line[1][1]+1))):
-            total += 1
-    return total
+        if line[1][0] <= line[0][1]:
+            p2 += 1
+    return p1, p2
 
 
 day = 4
@@ -27,8 +22,6 @@ if len(sys.argv) > 1:
     f = sys.argv[1]
 
 with open(f) as file:
-    data = [[[int(i) for i in l.split('-')] for l in line.strip().split(',')] for line in file]
-# data = heurparse(f)
+    data = [[[int(i) for i in r.split('-')] for r in line.strip().split(',')] for line in file]
 
-print(f'part1: {p1()}')
-print(f'part2: {p2()}')
+print(f'part1: %s\npart2: %s' % solve())
