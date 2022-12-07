32dbe6248be2963c657bf32dcf2021a8c6bc09fa
EyeDeck <eyedeck@gmail.com>
2022-12-07 01:10:56 -0500

    Solve 2022 day 7
    00:36:37 (2925) / 00:42:27 (2548)
    Today's puzzle was actually good!
    I decided it would be easiest to procedurally generate the file tree, then recursively traverse it, which turned out to be a good decision. Parsing/tree generation took like 25 minutes to write, then the rest of the problem was a piece of cake.

--- AoC2022/d6.py
+++ AoC2022/d6.py
@@ -1,8 +1,7 @@
 import sys
 
 
-def solve(p2):
-    l = 14 if p2 else 4
+def solve(l):
     for i in range(len(data)):
         if len(set(data[i:i+l])) == l:
             return i + l
@@ -16,5 +15,5 @@ if len(sys.argv) > 1:
 with open(f) as file:
     data = file.read().strip()
 
-print(f'part1: {solve(False)}')
-print(f'part2: {solve(True)}')
+print(f'part1: {solve(4)}')
+print(f'part2: {solve(14)}')
---
+++ AoC2022/d7.py
@@ -0,0 +1,64 @@
+import sys
+from collections import defaultdict
+
+
+def p1():
+    found = []
+    rtraverse(dirs, found, 100000)
+    return sum(found)
+
+
+def p2():
+    total_space = 70000000
+    needed_space = 30000000
+
+    found = []
+    rtraverse(dirs, found)
+    free_space = total_space - max(found)
+    return min([i for i in found if i >= needed_space - free_space])
+
+
+def rtraverse(d, found, tgt=-1):
+    acc = 0
+    for k, v in d.items():
+        if isinstance(v, int):
+            acc += v
+        else:
+            acc += rtraverse(v, found, tgt)
+    if tgt < 0 or acc <= tgt:
+        found.append(acc)
+    return acc
+
+
+day = 7
+f = f'd{day}.txt'
+if len(sys.argv) > 1:
+    f = sys.argv[1]
+
+with open(f) as file:
+    data = [line for line in file.read().split('$') if line]
+
+
+dirs = defaultdict(dict)
+curr = []
+for chunk in data:
+    chunk = [[word for word in line.split()] for line in chunk.strip().split('\n')]
+
+    if chunk[0][0] == 'cd':
+        d = chunk[0][1]
+        if d == '..':
+            curr.pop()
+        else:
+            curr.append(d)
+    else:
+        access = dirs
+        for level in curr:
+            access = access[level]
+        for line in chunk[1:]:
+            if line[0] == 'dir':
+                access[line[1]] = {}
+            else:
+                access[line[1]] = int(line[0])
+
+print(f'part1: {p1()}')
+print(f'part2: {p2()}')
