6983be06aaaca51e45ae91973e1399961689f25f
EyeDeck <eyedeck@gmail.com>
2022-12-10 02:50:10 -0500

    Wash 2022 day 10
    Fixed the -a / --animate flag always being on because of a logic error.
    
    Greatly simplified the interpreter logic by simply inserting a "fake" noop before every addx, which allows us to handle addx without having to directly meddle with the clock counter.

--- AoC2022/d10.py
+++ AoC2022/d10.py
@@ -6,40 +6,28 @@ from aoc import *
 def p1():
     cycles = {20, 60, 100, 140, 180, 220}
     x = 1
-    cycle = 1
     acc = 0
-    for line in data:
+    for cycle, op in enumerate(data, start=1):
         if cycle in cycles:
             acc += x * cycle
 
-        if line[0] == 'noop':
+        if op[0] == 'noop':
             pass
-        elif line[0] == 'addx':
-            cycle += 1
-
-            if cycle in cycles:
-                acc += x * cycle
-
-            x += line[1]
-        cycle += 1
+        elif op[0] == 'addx':
+            x += op[1]
     return acc
 
 
 def p2():
     screen = {(0, 0): '<', (39, 5): '>'}
     x = 1
-    cycle = 1
-    for line in data:
+    for cycle, op in enumerate(data, start=1):
         process(screen, cycle, x)
 
-        if line[0] == 'noop':
+        if op[0] == 'noop':
             pass
-        elif line[0] == 'addx':
-            cycle += 1
-
-            process(screen, cycle, x)
-            x += line[1]
-        cycle += 1
+        elif op[0] == 'addx':
+            x += op[1]
 
     print_2d('.', screen)
 
@@ -62,12 +50,13 @@ f = f'd{day}.txt'
 if len(sys.argv) > 1:
     f = sys.argv[1]
 
-animate = '-a' or '--animate' in sys.argv
+animate = ('-a' or '--animate') in sys.argv
 if animate:
     print('\033c \033[0;0H', end='')  # clear screen and set cursor to 0,0
 
 with open(f) as file:
-    data = [[int(word) if word.lstrip("-").isnumeric() else word for word in line.strip().split()] for line in file]
+    data = file.read().replace('addx', 'noop\naddx').split('\n')
+    data = [[int(word) if word.lstrip("-").isnumeric() else word for word in line.strip().split()] for line in data]
 
 print(f'part1: {p1()}')
 print('part2:')
