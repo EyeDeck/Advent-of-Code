0c63dc6dda029eb621da20e1a255e797ed8decde
EyeDeck <eyedeck@gmail.com>
2022-12-12 03:13:52 -0500

    Wash 2022 day 12
    Fixed my code to use the bfs implementation already in aoc.py; optimized part 2 somewhat (runs in a bit under 0.2s now).
    
    I could refactor part 2 so it searches from E to any 'a', with the bfs function tweaked a little and the neighbor adjacency thing flipped, and that would cut runtime down by a factor of like 4, but it's not really worth the effort.

--- AoC2022/d12.py
+++ AoC2022/d12.py
@@ -1,57 +1,26 @@
 from aoc import *
 
 
-def bfs(src, tgt, neighbors):
-    q = deque([src])
+def p1():
+    return len(bfs(source, destination, neighbors)) - 1
 
-    parent = {}
 
-    while q:
-        cur = q.popleft()
-        if cur == tgt:
-            break
+def p2():
+    srcs = [k for k, v in board.items() if v == 'a' and ('b' in [board[n] for n in neighbors[k]])]
+    # print_2d('.', {k: 'a' for k in srcs})
+    return min([len(bfs(s, destination, neighbors)) - 1 for s in srcs])
 
-        for n in neighbors(cur):
-            if n in parent:
-                continue
-            parent[n] = cur
-            q.append(n)
 
-    if tgt not in parent:
-        return None
-
-    pos = tgt
-    path = []
-    while pos != src:
-        path.append(pos)
-        pos = parent[pos]
-    path.append(src)
-    path.reverse()
-    return path
-
-@memo
 def get_neighbors(c):
     x, y = c
     adjacent = []
     for xm, ym in DIRS:
-        adj = (x+xm, y+ym)
+        adj = (x + xm, y + ym)
         if adj not in board:
             continue
         if ord(board[c]) + 1 >= ord(board[adj]):
             adjacent.append(adj)
-
     return adjacent
-
-
-def p1():
-    return len(bfs(src, dest, get_neighbors)) - 1
-
-
-def p2():
-    src = [k for k, v in board.items() if v == 'a']
-    paths = [bfs(s, dest, get_neighbors) for s in src ]
-    paths = [len(p)-1 for p in paths if p]
-    return min(paths)
 
 
 day = 12
@@ -63,13 +32,15 @@ board = {}
 with open(f) as file:
     for y, line in enumerate(file.readlines()):
         for x, c in enumerate(line.strip()):
-            board[x,y] = c
+            board[x, y] = c
+            if c == 'S':
+                board[x, y] = 'a'
+                source = (x, y)
+            elif c == 'E':
+                board[x, y] = 'z'
+                destination = (x, y)
 
-src = [k for k, v in board.items() if v == 'S'][0]
-dest = [k for k, v in board.items() if v == 'E'][0]
-
-board[src] = 'a'
-board[dest] = 'z'
+neighbors = {c: get_neighbors(c) for c in board}
 
 print(f'part1: {p1()}')
 print(f'part2: {p2()}')
