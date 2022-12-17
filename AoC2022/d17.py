6e6c6058ab3d2dd8b685e99a514057b8d0f1a4af
EyeDeck <eyedeck@gmail.com>
2022-12-17 18:45:38 -0500

    Improve 2022 day 17 part 2
    I had a good think about it today and worked out a better (possibly optimal?) way to predict cycle length. At least, it works on the example now too, which it hadn't before because the properties of the example and a real input are different.

--- AoC2022/d17.py
+++ AoC2022/d17.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from aoc import print_2d
 
@@ -95,6 +96,8 @@ def p1():
 
 
 def p2():
+    magic = 1000000000000
+
     max_y = 0
 
     last_y = 0
@@ -104,46 +107,42 @@ def p2():
     last_rock_diff = 0
 
     board = {(i, 0): '-' for i in range(-2, 5)}
-    # board.update({(-3, i): '|' for i in range(-5, 0)})
-    # board.update({(5, i): '|' for i in range(-5, 0)})
-    # print_2d('.', board)
-    # for rock_id in range(2022):
+
     data_index = 0
     rock_index = 0
+    cycle = len(data) * math.gcd(len(rocks), len(data))
 
-    stop_at = 100000000000
-    for rock_id in range(100000000):
+    cycle_index = cycle
+
+    stop_at = magic
+    for rock_id in range(magic):
         stop_at -= 1
         if stop_at == 0:
             break
-        # if rock_id == stop_at:
-        #     break
 
         for y in range(max_y - 6, max_y):
             board.update({(-3, y): '|', (5, y): '|'})
 
         rock, rock_index = next_rock(max_y, rock_index)
-        # print('new rock:')
-        # print_2d('.', board, rock)
 
         while True:
             next_jet, data_index = next_dir(data_index)
-            # print(next_jet)
 
-            if data_index == 0:  # and rock_index == 0:
-                # board[8, max_y] = '<'
+            if cycle_index == 0:
+                cycle_index = cycle
                 rock_diff = rock_id - last_rock
                 y_diff = max_y - last_y
                 print(f'rock# {rock_id}, last {last_rock}, diff {rock_diff};  h {max_y}, last {last_y}, diff {y_diff}')
                 # print('ct', rock_id, 'height', max_y, data_index, rock_index, 'diff', max_y - last_y)
                 last_rock = rock_id
                 last_y = max_y
-                # print(rock_diff, last_rock_diff, y_diff, last_y_diff)
                 if rock_diff == last_rock_diff and y_diff == last_y_diff:
-                    stop_at = (1000000000000 - rock_id) % rock_diff
-                    print('cycle detected', stop_at)
+                    stop_at = (magic - rock_id) % rock_diff
+                    print('cycle detected, doing another', stop_at, 'before calc')
                 last_y_diff = y_diff
                 last_rock_diff = rock_diff
+
+            cycle_index -= 1
 
             next_jet = -1 if next_jet == '<' else 1
             if can_move(rock, board, next_jet, 0):
@@ -157,21 +156,9 @@ def p2():
                 break
         board.update(rock)
         max_y = min(max_y, min(i[1] for i in rock.keys()))
-        # print(max_y)
-
-        # input()
-
-        # print(rock)
 
     # print_2d('.', board)
-
-    return -max_y + -(((1000000000000 - last_rock) // last_rock_diff) * last_y_diff)
-
-
-# 1514285714288
-# 1589142865526 too high
-# 1589142857183
-# 529411774004
+    return -max_y + -(((magic - last_rock) // last_rock_diff) * last_y_diff)
 
 
 day = 17
@@ -181,7 +168,6 @@ if len(sys.argv) > 1:
 
 with open(f) as file:
     data = [c for c in file.read().strip()]
-    # print(data)
 
 print('part1:', p1())
 print('part2:', p2())
