2b767510319391f5848bb9564fea648feb887326
EyeDeck <eyedeck@gmail.com>
2022-12-18 01:57:18 -0500

    Wash 2022 day 18
    Bounds for the flood fill in p2 are now calculated from the input, and also made a small optimization to part 1 to halve the number of adjacency checks needed.
    
    Good enough.

--- AoC2022/d18.py
+++ AoC2022/d18.py
@@ -2,103 +2,69 @@ import sys
 from collections import deque
 
 
-dirs = [
-    (0, 0, 1),
-    (0, 1, 0),
-    (1, 0, 0),
-    (0, 0, -1),
-    (0, -1, 0),
-    (-1, 0, 0)
-]
-
-
-def get_neighbors(x, y, z, b):
-    n = 0
-    for xo, yo, zo in dirs:
-        if (x + xo, y + yo, z + zo) in b:
-            n += 0
-        else:
-            n += 1
-    return n
-
-
 def p1():
-    cubes = set()
-    for x, y, z in data:
-        cubes.add((x, y, z))
-    acc = 0
-    for x, y, z in cubes:
-        acc += get_neighbors(x, y, z, cubes)
-    # print(cubes)
-    return acc
+    dirs = [
+        (0, 0, 1),
+        (0, 1, 0),
+        (1, 0, 0),
+    ]
 
+    def get_neighbors(x, y, z, b):
+        n = 0
+        for xo, yo, zo in dirs:
+            if (x + xo, y + yo, z + zo) in b:
+                n += 0
+            else:
+                # interestingly we can simply double-count positive directions, because for every
+                # double-counted face, there's exactly 1 opposite uncounted face, and it evens out
+                n += 2
+        return n
 
-def get_air(c, b):
-    x, y, z = c
-    empty = set()
-    filled = set()
-    for xo, yo, zo in dirs:
-        nx, ny, nz = (x + xo, y + yo, z + zo)
-        if nx < -2 or ny < -2 or nz < -2 or nx > 22 or ny > 22 or nz > 22:
-            continue
-        n = (nx, ny, nz)
-        if n in b:
-            filled.add(n)
-        else:
-            empty.add(n)
-    return empty, filled
-
-
-def bfs(src, b):
-    acc = 0
-    step = 0
-
-    q = deque([src])
-
-    seen = set()
-
-    while q:
-        step += 1
-        cur = q.popleft()
-
-        empty, filled = get_air(cur, b)
-
-        for n in empty:
-            d = (cur, n)
-            if d in seen:
-                continue
-            seen.add(d)
-            q.append(n)
-
-        for n in filled:
-            d = (cur, n)
-            if d not in seen:
-                seen.add(d)
-                acc += 1
-
-    return acc
-
-# def flood_fill(src, cubes):
-#     q = deque([src])
-#     seen = set()
-#     while q:
-#         cur = q.popleft()
-#         x,y,z = cur
-#         if x < -2 or y < -2 or z < -2 or x > 22 or y > 22 or z > 22:
-#             continue
-#
-#         air = []
-#         filled = []
-#
-#         for xo, yo, zo in dirs:
-#
+    return sum(get_neighbors(*c, cubes) for c in cubes)
 
 
 def p2():
-    cubes = set()
-    for x, y, z in data:
-        cubes.add((x, y, z))
+    dirs = [
+        (0, 0, 1),
+        (0, 1, 0),
+        (1, 0, 0),
+        (0, 0, -1),
+        (0, -1, 0),
+        (-1, 0, 0),
+    ]
 
+    def get_air(x,y,z, b):
+        empty = set()
+        filled = 0
+        for xo, yo, zo in dirs:
+            nx, ny, nz = (x + xo, y + yo, z + zo)
+            if nx < mn[0] or ny < mn[1] or nz < mn[2] or nx > mx[0] or ny > mx[1] or nz > mx[2]:
+                continue
+            n = (nx, ny, nz)
+            if n in b:
+                filled += 1
+            else:
+                empty.add(n)
+        return empty, filled
+
+    def bfs(src, b):
+        acc = 0
+        step = 0
+        q = deque([src])
+        seen = set()
+        while q:
+            step += 1
+            cur = q.popleft()
+
+            empty, filled = get_air(*cur, b)
+            acc += filled
+
+            for n in empty:
+                if n in seen:
+                    continue
+                seen.add(n)
+                q.append(n)
+        return acc
     return bfs((-1, -1, -1), cubes)
 
 
@@ -109,6 +75,13 @@ if len(sys.argv) > 1:
 
 with open(f) as file:
     data = [[int(i) for i in line.split(',')] for line in file]
+
+    cubes = set()
+    mn, mx, = [2**32, 2**32, 2**32], [0, 0, 0]
+    for x, y, z in data:
+        mn, mx = [min(mn[0], x), min(mn[1], y), min(mn[2], z)], [max(mx[0], x), max(mx[1], y), max(mx[2], z)]
+        cubes.add((x, y, z))
+    mn, mx = [i-2 for i in mn], [i+2 for i in mx]
 
 print('part1:', p1())
 print('part2:', p2())
