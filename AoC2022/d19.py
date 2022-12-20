7789d6acfbcf9acc9a8fb8c877c3b70f2d1467fc
EyeDeck <eyedeck@gmail.com>
2022-12-19 23:55:06 -0500

    Wash 2022 day 19
    I toyed around with some heuristics today and tried basically exactly the same one that I found to work well on day 16 part 2; after twiddling with the numbers for a while, I found some that solve both parts, AND the sample input correctly within a couple of seconds or so.

--- AoC2022/d19.py
+++ AoC2022/d19.py
@@ -1,60 +1,15 @@
+import heapq
 import sys
 import re
 from collections import *
 from operator import itemgetter
 
 robots = {
-    'g_rb': 'geode',
-    'b_rb': 'obsidian',
-    'c_rb': 'clay',
-    'o_rb': 'ore',
+    'bot_geode': 'geode',
+    'bot_obsidian': 'obsidian',
+    'bot_clay': 'clay',
+    'bot_ore': 'ore',
 }
-
-# def find_paths(inv, minute, costs):
-#     for robot, product in robots.items():
-#         # print(robot, product)
-#         if robot in inv:
-#             # print('added', product, 'from', robot)
-#             inv[product] += 1
-#
-#     if minute == 24:
-#         return inv
-#
-#     options = []
-#     for robot, cost in costs.items():
-#         if inv['ore'] >= cost[0] and inv['clay'] >= cost[1] and inv['obsidian'] >= cost[2]:
-#             options.append([robot, cost])
-#     # print(inv, minute) # , options)
-#     # input()
-#
-#     r = [find_paths(inv, minute+1, costs)]
-#     if options:
-#         for robot, cost in options:
-#             next_inv = inv.copy()
-#
-#             next_inv['ore'] -= cost[0]
-#             next_inv['clay'] -= cost[1]
-#             next_inv['obsidian'] -= cost[2]
-#
-#             next_inv[robot] += 1
-#
-#             r.append(find_paths(next_inv, minute+1, costs))
-#
-#     return r
-
-
-# def producible(inv, costs):
-#     options = {}
-#     for robot, cost in costs.items():
-#         cost_o, cost_c, cost_b = cost
-#         o = INF if cost_o == 0 else inv['ore'] // cost_o
-#         c = INF if cost_c == 0 else inv['clay'] // cost_c
-#         b = INF if cost_b == 0 else inv['obsidian'] // cost_b
-#         ct = min(o, c, b)
-#         if ct > 0:
-#             options[robot] = (ct, cost)
-#     # print('i', inv, 'c', costs, 'opt', options)
-#     return options
 
 
 def producible(inv, costs):
@@ -67,48 +22,53 @@ def producible(inv, costs):
     return options
 
 
+def get_weight_heur(inv):
+    return -sum(
+        [
+            # 1 * inv['ore'],
+            1 * inv['bot_ore'],
+            # 3 * inv['clay'],
+            20 * inv['bot_clay'],
+            # 5 * inv['obsidian'],
+            200 * inv['bot_obsidian'],
+            500 * inv['geode'],
+            10000 * inv['bot_geode'],
+        ]
+    )
+
+
 def find_paths(inv, costs, minutes, s):
-    paths = [inv]
+    paths = []
+    heapq.heappush(paths, [0, 0, inv])
     seen = set()
 
     most_ore = max(costs.values(), key=itemgetter(0))[0]
-    most_clay = costs['b_rb'][1]
-    most_obsidian = costs['g_rb'][2]
+    most_clay = costs['bot_obsidian'][1]
+    most_obsidian = costs['bot_geode'][2]
 
     all_keys = tuple(r for r in robots.keys()) + tuple(r for r in robots.values())
 
-    # print(most_ore, most_clay, most_obsidian)
-
-    most_geodes = 0
+    heap_id = 0
 
     for minute in range(minutes):
-        print(f'{s}: {minute+1}/{minutes}, paths: {len(paths)}', end='\r')
+        print(f'{s}: {minute + 1}/{minutes}, paths: {len(paths)}', end='\r')
         new_paths = []
 
         while paths:
-            inv = paths.pop()
+            _, _, inv = heapq.heappop(paths)
 
             options = producible(inv, costs)
 
             for robot, product in robots.items():
-                # print(robot, product)
                 if robot in inv:
-                    # print('added', product, 'from', robot)
                     inv[product] += inv[robot]
 
-            if inv['geode'] < most_geodes:
-                # print('dropped', inv)
-                continue
-            most_geodes = max(most_geodes, inv['geode'])
-
-            # print(minute, inv, options)
-            # input()
-
             # if we can make a geode bot, only do that
-            if 'g_rb' in options:
-                options = {'g_rb': options['g_rb']}
+            if 'bot_geode' in options:
+                options = {'bot_geode': options['bot_geode']}
             else:
-                new_paths.append(inv)
+                heap_id -= 1
+                heapq.heappush(new_paths, [get_weight_heur(inv), heap_id, inv])
 
             if options:
                 for robot, cost in options.items():
@@ -122,15 +82,15 @@ def find_paths(inv, costs, minutes, s):
                     next_bot_ct = next_inv[robot] + 1
                     next_inv[robot] = next_bot_ct
 
-                    if robot == 'o_rb':  # ore robot
+                    if robot == 'bot_ore':  # ore robot
                         # don't produce more than any robot uses
                         if next_bot_ct > most_ore:
                             continue
-                    elif robot == 'c_rb':  # clay robot
+                    elif robot == 'bot_clay':  # clay robot
                         # don't produce more than obsidian robot uses
                         if next_bot_ct > most_clay:
                             continue
-                    elif robot == 'b_rb':  # obsidian robot
+                    elif robot == 'bot_obsidian':  # obsidian robot
                         # don't produce more than geode robot uses
                         if next_bot_ct > most_obsidian:
                             continue
@@ -140,46 +100,32 @@ def find_paths(inv, costs, minutes, s):
                         continue
                     seen.add(hashable)
 
-                    # if robot == 'g_rb':
-                    #     most_geodes = max(next_inv[robot])
-
-                    new_paths.append(next_inv)
-
-        # for path in new_paths:
-        #     print(path)
-        paths = new_paths
-        # input()
+                    heap_id -= 1
+                    heapq.heappush(new_paths, [get_weight_heur(next_inv), heap_id, next_inv])
+        for _ in range(min(len(new_paths), 1000)):
+            heapq.heappush(paths, heapq.heappop(new_paths))
     print(' ' * 50, end='\r')
-    return max(paths, key=itemgetter('geode'))
+    return max(paths, key=lambda x: x[2]['geode'])[2]['geode']
 
 
 def p1():
     acc = 0
-    for id, bp in enumerate(data):
-        costs = {'o_rb': (bp[0], 0, 0), 'c_rb': (bp[1], 0, 0), 'b_rb': (bp[2], bp[3], 0), 'g_rb': (bp[4], 0, bp[5])}
-        # print(id, costs)
-
+    for i, cost in enumerate(costs):
         start_inv = defaultdict(int)
-        start_inv['o_rb'] = 1
+        start_inv['bot_ore'] = 1
 
-        acc += (id+1) * find_paths(start_inv, costs, 24, f'{id+1}/{len(data)+1}')['geode']
-        # die()
-
+        acc += (i + 1) * find_paths(start_inv, cost, 24, f'part 1: {i + 1}/{len(data) + 1}')
     return acc
 
 
 def p2():
     acc = 1
-    for id, bp in enumerate(data[:3]):
-        costs = {'o_rb': (bp[0], 0, 0), 'c_rb': (bp[1], 0, 0), 'b_rb': (bp[2], bp[3], 0), 'g_rb': (bp[4], 0, bp[5])}
-        print(id, costs)
-
+    for i, cost in enumerate(costs[:3]):
         start_inv = defaultdict(int)
-        start_inv['o_rb'] = 1
+        start_inv['bot_ore'] = 1
 
-        acc *= find_paths(start_inv, costs, 32, f'{id + 1}/3')['geode']
-        # die()
-
+        n = find_paths(start_inv, cost, 32, f'part 2: {i + 1}/3')
+        acc *= n
     return acc
 
 
@@ -190,6 +136,10 @@ if len(sys.argv) > 1:
 
 with open(f) as file:
     data = [[int(i) for i in re.findall(r'[0-9]+', line)][1:] for line in file]
+
+costs = []
+for bp in data:
+    costs.append({'bot_ore': (bp[0], 0, 0), 'bot_clay': (bp[1], 0, 0), 'bot_obsidian': (bp[2], bp[3], 0), 'bot_geode': (bp[4], 0, bp[5])})
 
 print('part1:', p1())
 print('part2:', p2())
