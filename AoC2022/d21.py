66ad5ac724f81956e425c125e59c73e1bdb0e456
EyeDeck <eyedeck@gmail.com>
2022-12-21 01:37:27 -0500

    Wash 2022 day 21
    Part 2 has a solver now, which works for my input but I make zero guarantee it'll work for any other input.

--- AoC2022/d21.py
+++ AoC2022/d21.py
@@ -3,49 +3,39 @@ import operator
 import sys
 
 
-def p1():
-    monkeys = copy.deepcopy(data)
-    while not isinstance(monkeys['root'], int):
-        for name, d in monkeys.items():
-            if isinstance(d, int):
-                pass
-            else:
-                l, op, r = d
-                # print(l, op, r)
+def solve(monkeys):
+    while isinstance(monkeys['root'], list):
+        for monkey_name, monkey_data in monkeys.items():
+            if isinstance(monkey_data, list):
+                l, op, r = monkey_data
                 if isinstance(monkeys[l], int) and isinstance(monkeys[r], int):
-                    # print(l, r)
                     l, r = monkeys[l], monkeys[r]
-                    monkeys[name] = op(l, r)
+                    monkeys[monkey_name] = op(l, r)
+                    # if monkey_name == 'root':
+                    #     print(l, r, l > r, monkeys['root'])
     return monkeys['root']
 
 
 def p2():
-    next_i = 1
-    while True:
-        # print(next_i, end='\r')
-        monkeys = copy.deepcopy(data)
-        monkeys['root'][1] = operator.eq
-        monkeys['humn'] = next_i
-        while not isinstance(monkeys['root'], int):
-            for name, d in monkeys.items():
-                if isinstance(d, int):
-                    pass
-                else:
-                    l, op, r = d
-                    # print(l, op, r)
-                    if isinstance(monkeys[l], int) and isinstance(monkeys[r], int):
-                        # print(l, r)
-                        l, r = monkeys[l], monkeys[r]
-                        monkeys[name] = op(l, r)
+    # work out the correct uppermost bit
+    place = 1
+    data['root'][1] = operator.ge
+    data['humn'] = place
+    while solve(copy.deepcopy(data)):
+        place += 1
+        data['humn'] = 1 << place
+    n = 1 << (place-1)
 
-                        if monkeys['root'] == True:
-                            return next_i
+    # now binary search the lower bits
+    data['root'][1] = operator.lt
+    for i in range(place, -1, -1):
+        if i == 0:
+            data['root'][1] = operator.eq
 
-                        if name == 'root':
-                            print(l, r, l>r) # , end='\r')
-                            # next_i = l // 2
-                            next_i = int(input('next? '))
-
+        data['humn'] = n + (1 << i)
+        if not solve(copy.deepcopy(data)):
+            n += (1 << i)
+    return n
 
 
 day = 21
@@ -71,5 +61,5 @@ with open(f) as file:
         else:
             data[name] = [parts[1], operators[parts[2]], parts[3]]
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1:', solve(copy.deepcopy(data)))
+print('part2:', p2())
