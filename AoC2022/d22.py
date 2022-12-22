25dc9f1a8288f95c5eb5d394f247e9ff95be798f
EyeDeck <eyedeck@gmail.com>
2022-12-22 05:55:10 -0500

    Solve 2022 day 22 part 2
    00:43:07 (871) / 05:40:10 (2233)
    I tried to make a generalized solution for part 2, but realized I had no idea how to do that in a reasonable time, so I made a paper cube, and spent a lot of time staring at it in order to hardcode the edge "teleports" based on the real input's specific layout
    
    Part 2 also requires some modification to the input, which manually defines which faces are which in a completely arbitrary order (based on what I wrote on my paper cube), and also includes a move interpreter I used to navigate by hand for debugging.
    
    While satisfying, that was also incredibly tedious, and I definitely don't have it in me to go back and wash this one right now.

--- AoC2022/d22.py
+++ AoC2022/d22.py
@@ -13,6 +13,7 @@ DIRS = [
     (0, -1),  # u
 ]
 DIRS_C = ['>', 'v', '<', '^']
+DIRS_R = {'>':0, 'v':1, '<':2, '^':3}
 
 turn_map = {
     'R': 1,
@@ -44,8 +45,8 @@ def p1():
 
                     nx = vadd(nx, DIRS[heading])
 
-                print('hit a wall')
                 if board[nx] == '#':
+                    print('hit a wall')
                     break
 
                 path[nx] = DIRS_C[heading]
@@ -128,82 +129,175 @@ def get_chunk(board, seen, cur, s):
     return face
 
 
+# output: next face, next heading, flip_x, flip_y, swap_axes
 warp_map = {
-    (1, '<'): (5, '>', -1),
-    (1, '^'): (6, '>', 1),
+    (1, '<'): (5, '>', False, True, False),
+    (1, '^'): (6, '>', False, False, True),
     # (1, '>'): (2, '>'),
     # (1, 'v'): (3, 'v'),
 
     # (2, '<'): (1, '<'),
-    (2, '^'): (6, '^', 1),
-    (2, '>'): (4, '<', -1),
-    (2, 'v'): (3, '<', 1),
+    (2, '^'): (6, '^', False, True, False),
+    (2, '>'): (4, '<', False, True, False),
+    (2, 'v'): (3, '<', False, False, True),
 
-    (3, '<'): (5, 'v', 1),
+    (3, '<'): (5, 'v', False, False, True),
     # (3, '^'): (1, '^'),
-    (3, '>'): (2, '^', 1),
+    (3, '>'): (2, '^', False, False, True),
     # (3, 'v'): (4, 'v'),
 
     # (4, '<'): (5, '<'),
     # (4, '^'): (3, '^'),
-    (4, '>'): (2, '<', -1),
-    (4, 'v'): (6, '<', 1),
+    (4, '>'): (2, '<', False, True, False),
+    (4, 'v'): (6, '<', False, False, True),
 
-    (5, '<'): (1, '>', -1),
-    (5, '^'): (3, '>', 1),
+    (5, '<'): (1, '>', False, True, False),
+    (5, '^'): (3, '>', False, False, True),
     # (5, '>'): (4, '>'),
     # (5, 'v'): (6, 'v'),
 
-    (6, '<'): (1, 'v', 1),
+    (6, '<'): (1, 'v', False, False, True),
     # (6, '^'): (5, '^'),
-    (6, '>'): (4, '^', 1),
-    (6, 'v'): (2, 'v', 1),
+    (6, '>'): (4, '^', False, False, True),
+    (6, 'v'): (2, 'v', False, True, False),
 }
 
+# warp_map = {
+#     (1, '<'): lambda x, y: (5, '>', -x, -y),
+#     (1, '<'): (5, '>', -1),
+#     (1, '^'): (6, '>', 1),
+#     # (1, '>'): (2, '>'),
+#     # (1, 'v'): (3, 'v'),
+#
+#     # (2, '<'): (1, '<'),
+#     (2, '^'): (6, '^', 1),
+#     (2, '>'): (4, '<', -1),
+#     (2, 'v'): (3, '<', 1),
+#
+#     (3, '<'): (5, 'v', 1),
+#     # (3, '^'): (1, '^'),
+#     (3, '>'): (2, '^', 1),
+#     # (3, 'v'): (4, 'v'),
+#
+#     # (4, '<'): (5, '<'),
+#     # (4, '^'): (3, '^'),
+#     (4, '>'): (2, '<', -1),
+#     (4, 'v'): (6, '<', 1),
+#
+#     (5, '<'): (1, '>', -1),
+#     (5, '^'): (3, '>', 1),
+#     # (5, '>'): (4, '>'),
+#     # (5, 'v'): (6, 'v'),
+#
+#     (6, '<'): (1, 'v', 1),
+#     # (6, '^'): (5, '^'),
+#     (6, '>'): (4, '^', 1),
+#     (6, 'v'): (2, 'v', 1),
+# }
 
-def p2():
-    face_size = 4
 
-    cur = (0, 0)
-    heading = 0
-    path = {}
-    while True:
-        if cur in board:
-            break
-        cur = vadd(cur, (1, 0))
-    top_l = cur
-    # front, right, back, left, up, down
-    face_map = {}
-
-    seen = set()
-    cube_map = get_chunk(board, seen, cur, face_size)
-
-    # print(cube_map)
-    for k, v in cube_map.items():
-        print(k, v)
-        print_2d(' ', v)
-
-    # for y in range(cur[1], face_size):
-    #     for x in range(cur[0], face_size):
-    #         faces[0][x,y] = board[x,y]
-    #         del board[x,y]
-    # faces[0] =
-    # print_2d(' ', faces[0])
-
-    return None
+# def p2():
+#     face_size = 4
+#
+#     cur = (0, 0)
+#     heading = 0
+#     path = {}
+#     while True:
+#         if cur in board:
+#             break
+#         cur = vadd(cur, (1, 0))
+#     top_l = cur
+#     # front, right, back, left, up, down
+#     face_map = {}
+#
+#     seen = set()
+#     cube_map = get_chunk(board, seen, cur, face_size)
+#
+#     # print(cube_map)
+#     for k, v in cube_map.items():
+#         print(k, v)
+#         print_2d(' ', v)
+#
+#     # for y in range(cur[1], face_size):
+#     #     for x in range(cur[0], face_size):
+#     #         faces[0][x,y] = board[x,y]
+#     #         del board[x,y]
+#     # faces[0] =
+#     # print_2d(' ', faces[0])
+#
+#     return None
 
 
 def get_warp(c, h):
-    print('warping', c, h)
-    print('current face', faces[c])
-    print('current dir', DIRS_C[h])
-    next_key = (int(faces[c]), DIRS_C[h])
-    print(next_key)
-    print('next edge', warp_map[next_key])
-    new_coord = c
-    new_heading = h
+    # print('warping', c, h)
+    curr_face = int(faces[c])
+    # print('current face', faces[c])
+    # print('current dir', DIRS_C[h])
+    next_key = (curr_face, DIRS_C[h])
+    # print(next_key)
+    # print('face_bounds[curr_face][0]', face_bounds[curr_face][0],'face_bounds[curr_face][1]', face_bounds[curr_face][1])
 
-    return new_coord, new_heading
+    norm_x, norm_y = c[0] - face_bounds[curr_face-1][0], c[1] - face_bounds[curr_face-1][1]
+    # print('norm_x, norm_y', norm_x, norm_y)
+
+    next_face, next_dir, flip_x, flip_y, swap_axes = warp_map[next_key]
+
+    if swap_axes:
+        next_x, next_y = norm_y, norm_x
+    else:
+        next_x, next_y = norm_x, norm_y
+
+    # print('next_face, next_dir, next_x, next_y', next_face, next_dir, flip_x, flip_y)
+    next_x_min, next_y_min, next_x_max, next_y_max = face_bounds[next_face-1]
+
+    if not flip_x:
+        next_x = next_x_min + next_x
+    else:
+        next_x = next_x_max - next_x
+
+    if not flip_y:
+        next_y = next_y_min + next_y
+    else:
+        next_y = next_y_max - next_y
+
+    next_coord = (next_x, next_y)
+
+    # if next_dir == '>':
+    #     if flipped:
+    #         next_coord = next_x_min + norm_x, next_y_max - norm_y
+    #         print('aaaaa')
+    #     else:
+    #         next_coord = next_x_min + norm_x, next_y_min + norm_y
+    # elif next_dir == 'v':
+    #     if flipped:
+    #         next_coord = next_x_min + norm_y, next_x_max - norm_x
+    #     else:
+    #         next_coord = next_x_min + norm_y, next_x_min + norm_x
+    # elif next_dir == '<':
+    #     if flipped:
+    #         next_coord = next_x_min + norm_x, next_y_max - norm_y
+    #     else:
+    #         next_coord = next_x_min + norm_x, next_y_min + norm_y
+    # elif next_dir == '^':
+    #     if flipped:
+    #         next_coord = next_x_min + norm_y, next_x_max - norm_x
+    #     else:
+    #         next_coord = next_x_min + norm_y, next_x_min + norm_x
+
+    # if next_dir in '<>':
+    #     if flipped:
+    #         next_coord = next_x_min + norm_x, next_y_max - norm_y  # correct ?
+    #     else:
+    #         next_coord = next_x_min + norm_x, next_y_min + norm_y
+    # elif next_dir in '^v':
+    #     if flipped:
+    #         next_coord = next_x_max - norm_x, next_x_min + norm_y
+    #     else:
+    #         next_coord = next_x_min + norm_x, next_x_min + norm_y
+
+    print('goto', next_coord, next_dir)
+    return next_coord, DIRS_R[next_dir]
+
 
 def p2():
     cur = (0, 0)
@@ -213,18 +307,23 @@ def p2():
         if cur in board:
             break
         cur = vadd(cur, (1, 0))
-    for d in dirs:
-        print(d)
+    # for d in dirs:
+    while dirs:
+        d = dirs.pop(0)
+        print(f'current pos {cur}, heading {DIRS_C[heading]}, next input:', d)
         if isinstance(d, int):
             path[cur] = DIRS_C[heading]
             for i in range(d):
                 nx = vadd(cur, DIRS[heading])
+                nx_h = heading
                 if nx not in board:
                     nx, nx_h = get_warp(cur, heading)
 
-                print('hit a wall')
                 if board[nx] == '#':
+                    print('hit a wall')
                     break
+
+                heading = nx_h
 
                 path[nx] = DIRS_C[heading]
 
@@ -236,11 +335,19 @@ def p2():
             # print(cs)
             # print_2d(' ', board, path) # , constrain = cs)
             # input()
-        else:
-            heading += 1 if d == 'R' else -1
+        elif d and d in 'LRlr':
+            heading += 1 if d in 'Rr' else -1
             heading %= 4
 
             print('heading=', heading, '(', DIRS_C[heading], ')')
+        else:
+            pass
+            # print('invalid input')
+
+        if not dirs:
+            print_2d('  ', board, path, {cur: '@'})
+
+            dirs.extend(int(i) if i.isnumeric() else i for i in input('moves: ').split())
 
     print_2d(' ', board, path, {cur: '@'})
 
@@ -249,6 +356,7 @@ def p2():
     print(col, row, DIRS_C[heading])
     return row * 1000 + col * 4 + heading
 
+# 95275 too low
 
 day = 22
 f = f'd{day}.txt'
@@ -266,13 +374,19 @@ for y, line in enumerate(map_raw.splitlines()):
         board[x, y] = c
 
 faces = {}
+face_whatever = [set() for _ in range(6)]
 for y, line in enumerate(face_raw.splitlines()):
     for x, c in enumerate(line):
         if c == ' ':
             continue
         faces[x, y] = c
+        # print(c)
+        face_whatever[int(c)-1].add((x,y))
 
-face_whatever = [{} for _ in range(6)]
+# print(face_whatever)
+
+face_bounds = [(min(s, key=itemgetter(0))[0], min(s, key=itemgetter(1))[1], max(s, key=itemgetter(0))[0], max(s, key=itemgetter(1))[1], ) for s in face_whatever]
+print(face_bounds)
 
 dirs = [int(i) if i.isnumeric() else i for i in dir_raw.replace('R', ' R ').replace('L', ' L ').split(' ')]
 
