933b72c1734934fe8657eb9e2b86e518bbc9d686
EyeDeck <eyedeck@gmail.com>
2023-01-16 09:04:05 -0500

    Wash 2018 day 15
    Squished the separate tick and solve functions, because having them separated added a bunch of annoying overhead for no benefit.

--- AoC2018/d15.py
+++ AoC2018/d15.py
@@ -60,7 +60,7 @@ def bfs(src, cbt, grid):
 
     target_candidates = [c for c in parent if c in cbt and c != src and cbt[c].type != start_type]
 
-    paths_dict = {}
+    # paths_dict = {}
     paths = []
     i = 0
     for tgt in target_candidates:
@@ -72,11 +72,9 @@ def bfs(src, cbt, grid):
             pos = parent[pos]
         path.append(src)
         path.reverse()
-
-        paths_dict.update({k:chr(96+i) for k in path})
         paths.append(path)
+        # paths_dict.update({k:chr(96+i) for k in path})
 
-    # print(paths)
     paths = [path for path in paths if len(path) == len(min(paths, key=len))]
 
     # rev = {(0, -1): 'v', (-1, 0): '>', (1, 0):  '<', (0, 1):  '^'}
@@ -85,53 +83,6 @@ def bfs(src, cbt, grid):
 
     # print(paths)
     return sorted(paths, key=lambda x:(x[-1][1], x[-1][0]))[0]
-
-
-def tick(cbts, board, p2=False):
-    combatants = sorted_dict(cbts, key=lambda x: (x[1], x[0]))
-    turn_order = [*combatants]
-
-    for pos in turn_order:
-        if pos not in combatants:
-            continue
-
-        combatant = combatants[pos]
-
-        # print(f'ticking {combatant}')
-
-        neighbors = get_neighbors(pos, combatants, board)
-        neighbors = {k: v for k, v in neighbors.items() if isinstance(v, Combatant) and combatant.type != v.type}
-        if not neighbors:
-            # print('moving')
-            path = bfs(pos, combatants, board)
-            if path is None:
-                continue
-
-            # print(path)
-            # print_2d('  ', board, {k: '~' for k in path}, combatants)
-            next_tile = path[1]
-            combatant.pos = next_tile
-            combatants[next_tile] = combatant
-            del combatants[pos]
-
-            neighbors = get_neighbors(next_tile, combatants, board)
-            neighbors = {k: v for k, v in neighbors.items() if isinstance(v, Combatant) and combatant.type != v.type}
-
-        if neighbors:  # attacking
-            neighbors = sorted([combatants[c] for c in neighbors], key=lambda x: x.hp)
-            target = neighbors[0]
-            # print('attacking', target)
-
-            alive = target.hit(combatant.dmg)
-            if not alive:
-                del combatants[target.pos]
-
-                if p2 and target.type == 'E':
-                    return None, False, False
-                elif len(set(c.type for c in combatants.values())) == 1:
-                    return combatants, pos == turn_order[-1], True
-
-    return combatants, True, True
 
 
 def solve(elf_dmg=3, p2=False):
@@ -143,21 +94,53 @@ def solve(elf_dmg=3, p2=False):
     # print_2d('. ', board, combatants)
     # print()
 
-    i = 0
-    while True:
-        combatants, last, valid = tick(combatants, board, p2)
-        if not valid:
-            return None
+    for i in range(200):
+        combatants = sorted_dict(combatants, key=lambda x: (x[1], x[0]))
+        turn_order = [*combatants]
+
+        for pos in turn_order:
+            if pos not in combatants:
+                continue
+
+            combatant = combatants[pos]
+
+            # check for adjacent targets
+            neighbors = get_neighbors(pos, combatants, board)
+            neighbors = {k: v for k, v in neighbors.items() if isinstance(v, Combatant) and combatant.type != v.type}
+
+            # move
+            if not neighbors:
+                path = bfs(pos, combatants, board)
+                if path is None:
+                    continue
+
+                # print_2d('  ', board, {k: '~' for k in path}, combatants)
+                next_tile = path[1]
+                combatant.pos = next_tile
+                combatants[next_tile] = combatant
+                del combatants[pos]
+
+                # recheck for adjacent targets
+                neighbors = get_neighbors(next_tile, combatants, board)
+                neighbors = {k: v for k, v in neighbors.items() if isinstance(v, Combatant) and combatant.type != v.type}
+
+            # attack
+            if neighbors:
+                neighbors = sorted([combatants[c] for c in neighbors], key=lambda x: x.hp)
+                target = neighbors[0]
+
+                alive = target.hit(combatant.dmg)
+                if not alive:
+                    del combatants[target.pos]
+
+                    if p2 and target.type == 'E':
+                        return None
+                    elif len(set(c.type for c in combatants.values())) == 1:
+                        return (i + (1 if pos == turn_order[-1] else 0)) * sum(c.hp for c in combatants.values())
 
         # print(f'After {i} round{"s" if i > 1 else ""}:')
         # print_2d('. ', board, combatants)
         # print([str(c) for c in combatants.values()], '\n')
-        # input((set(c.type for c in combatants.values())))
-
-        if len(set(c.type for c in combatants.values())) == 1:
-            return (i + (1 if last else 0)) * sum(c.hp for c in combatants.values())
-
-        i += 1
 
 
 def p2():
