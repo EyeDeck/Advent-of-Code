36b37a17312c9dd05f988e756813da8a568ec156
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-01 01:44:23 -0500

    Scrub 2023 day 1 a little harder

--- AoC2023/d1.py
+++ AoC2023/d1.py
@@ -1,15 +1,14 @@
 from aoc import *
 
 def firstnum(line, reverse, replace):
-    rpl = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0}
     i = len(line)-1 if reverse else 0
     while (reverse and i >= 0) or (i <= len(line)):
         if line[i].isdigit():
             return line[i]
         if replace:
-            for k, v in rpl.items():
-                if line[i:i + len(k)] == k:
-                    return str(v)
+            for n, v in enumerate(num_strs):
+                if line[i:i + len(v)] == v:
+                    return str(n)
         i = i - 1 if reverse else i + 1
     die()
 
@@ -23,6 +22,7 @@ def solve(replace):
 setday(1)
 
 data = parselines()
+num_strs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 print('part1:', solve(False) )
 print('part2:', solve(True) )
