a6968a8f78b46369e04a88f8af8ed5bd37d33ba5
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-02 13:08:38 -0500

    Wash 2023 day 2
    Realized that the code for part 2 could also be used to solve part 1 efficiently

--- AoC2023/d2.py
+++ AoC2023/d2.py
@@ -2,38 +2,14 @@ import math
 
 from aoc import *
 
-def is_valid(sets):
+def solve():
     target = {'red': 12, 'green': 13, 'blue': 14}
-    for cubes in sets:
-        for cube in cubes:
-            ct, color = cube.split()
-            ct = int(ct)
-            # print(color, ct)
-            if color not in target:
-                return False
-            if ct > target[color]:
-                return False
-    return True
 
-
-def p1():
-    acc = 0
-
-    for line in data:
-        game, sets = line.split(':')
-        sets = [[x.strip() for x in s.strip().split(',')] for s in sets.split(';')]
-        # print(game)
-        if is_valid(sets):
-            acc += int(game.split()[-1])
-        # l = [word.strip().strip(',') for word in line.split()]
-        # print(l)
-    return acc
-
-
-def p2():
-    acc = 0
+    acc_1 = 0
+    acc_2 = 0
     for line in data:
         minimums = defaultdict(int)
+
         game, sets = line.split(':')
         sets = [[x.strip() for x in s.strip().split(',')] for s in sets.split(';')]
         for cubes in sets:
@@ -41,14 +17,19 @@ def p2():
                 ct, color = cube.split()
                 ct = int(ct)
                 minimums[color] = max(minimums[color], ct)
-        print(minimums)
-        acc += math.prod(minimums.values())
-    return acc
+
+        for k,v in minimums.items():
+            if k not in target or v > target[k]:
+                break
+        else:
+            acc_1 += int(game.split()[-1])
+
+        acc_2 += math.prod(minimums[k] for k in target.keys())
+    return acc_1, acc_2
 
 
 setday(2)
 
 data = parselines()
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1: %s\npart2: %s' % solve())
