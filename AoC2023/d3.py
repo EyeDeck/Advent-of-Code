c67cef5e530ece696748f19226026dd59fc2b911
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-03 01:24:08 -0500

    Wash 2023 day 3 kinda
    it's still smelly but I can't be bothered

--- AoC2023/d3.py
+++ AoC2023/d3.py
@@ -1,37 +1,14 @@
 import math
-import sys
-import re
-# import numpy as np
-from collections import defaultdict  # defaultdict(int)
-import functools  # @functools.cache
-from collections import *
-from math import *
-from pprint import pprint
-
 from aoc import *
-
-
-# print_2d(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# print_2d_repl(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# INF = sys.maxsize
-# @memo
-# splat(f)
-# bfs(src, tgt, {n1: [n2, n3, n4]})
-# wbfs(src, tgt, edge_func)
-# astar(src, tgt, edge_func, heur_func)
-# ccs(srcs, neigh_func, filt_func?)
-# Dedup().seen(x)
-
 
 def p1():
     def has_symbol():
-        for x in range(bb[0][0], bb[1][0]):
+        for x in range(bb[0][0], bb[1][0] + 1):
             for y in range(bb[0][1], bb[1][1] + 1):
                 if (x,y) not in grid:
                     continue
                 adj = grid[(x,y)]
                 if adj != '.' and not adj.isnumeric():
-                    # print('asd')
                     return True
         return False
 
@@ -47,26 +24,21 @@ def p1():
             while coord in grid and grid[coord].isnumeric():
                 num_str += grid[coord]
                 coord = vadd(coord, (1,0))
-            bb = (vadd(k, (-1,-1)), vadd(coord, (1,1)))
-            # print(num_str, bb)
+
+            bb = (vadd(k, (-1,-1)), vadd(coord, (0,1)))
 
             if has_symbol():
-                # print(num_str)
                 acc += int(num_str)
-            # else:
-            #     print('not', num_str)
-
     return acc
-# not 548609
 
-
+# this is hideous but I just don't care enough to fix it
 def p2():
     acc = 0
     for k,v in grid.items():
         if v != '*':
             continue
         numeric = {}
-        # print(k,v)
+
         bb = (vadd(k, (-1,-1)), vadd(k, (1,1)))
         for x in range(bb[0][0], bb[1][0] + 1):
             for y in range(bb[0][1], bb[1][1] + 1):
@@ -78,9 +50,9 @@ def p2():
         for k,v in numeric.items():
             if k in seen:
                 continue
+
             while k in grid and grid[k].isnumeric():
                 k = vadd(k, (-1,0))
-            # print('left = ', k)
 
             num_str = ''
             k = vadd(k, (1, 0))
@@ -88,33 +60,17 @@ def p2():
                 seen.add(k)
                 num_str += grid[k]
                 k = vadd(k, (1,0))
-                # print('at', k, num_str)
 
             adj_numbers.append(int(num_str))
-        # print(adj_numbers)
-            # # print(coord)
-            # while coord in grid and grid[coord].isnumeric():
-            #     # print(coord)
-            #     numeric_lr[coord] = grid[coord]
-            #     coord = vadd(coord, (1,0))
-            #
-            # coord = k
 
         if len(adj_numbers) == 2:
-            # print(adj_numbers)
             acc += math.prod(adj_numbers)
-
-
-
-        # print(numeric_lr)
 
     return acc
 
 
 setday(3)
 
-data = parselines()
-# data = parselines(get_ints)
 grid, inverse, unique = parsegrid()
 
 print('part1:', p1() )
