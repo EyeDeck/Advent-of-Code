e1dbac211a1e46c0d1a69ee4fcd232dfe0e809bc
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-07 02:15:16 -0500

    Wash 2023 day 7

--- AoC2023/d7.py
+++ AoC2023/d7.py
@@ -2,93 +2,44 @@ import collections
 from aoc import *
 
 
-def p1():
-    def get_type(cards):
-        counted = collections.Counter(cards)
-        # print(counted)
-        cts = [counted[k] for k in counted.keys()]
-        cts.sort()
-        cts.reverse()
-        # print(cts)
-        if cts[0] == 5:
-            return 0  # Five of a kind
-        elif cts[0] == 4:
-            return 1  # Four of a kind
-        elif cts[0] == 3:
-            if cts[1] == 2:
-                return 2  # Full house
-            else:
-                return 3  # Three of a kind
-        elif cts[0] == 2:
-            if cts[1] == 2:
-                return 4  # Two pair
-            else:
-                return 5  # One pair
-        else:
-            return 6
+def get_type(cards, p2=False):
+    counted = collections.Counter(cards)
+    cts = [counted[k] for k in counted.keys()]
+    cts.sort(reverse=True)
 
-    repl = {c:string.ascii_lowercase[i] for i,c in enumerate('AKQJT98765432')}
-    # print(repl)
+    if p2 and 'J' in counted and cts[0] != 5:
+        cts.remove(counted['J'])
+        cts[0] += counted['J']
+
+    if cts[0] == 5:
+        return 0  # Five of a kind
+    elif cts[0] == 4:
+        return 1  # Four of a kind
+    elif cts[0] == 3:
+        if cts[1] == 2:
+            return 2  # Full house
+        else:
+            return 3  # Three of a kind
+    elif cts[0] == 2:
+        if cts[1] == 2:
+            return 4  # Two pair
+        else:
+            return 5  # One pair
+    else:
+        return 6
+
+
+def solve(p2):
+    order = 'AKQT98765432J' if p2 else 'AKQJT98765432'
+    repl = {c: string.ascii_lowercase[i] for i, c in enumerate(order)}
     hands = []
     for line in data:
         cards, bid = line.split()
-        type = get_type(cards)
-        processed = str(type) + ''.join(repl[k] for k in cards)
-        hands.append((processed, int(bid)))
-    hands.sort()
-    hands.reverse()
-    acc = 0
-    for i, (cards, bid) in enumerate(hands):
-        # print(i, cards, bid)
-        acc += (i+1) * bid
-    return acc
-
-# not 251236888
-
-
-def p2():
-    def get_type(cards):
-        counted = collections.Counter(cards)
-        print(counted)
-        cts = [counted[k] for k in counted.keys()]
-        cts.sort()
-        cts.reverse()
-        print(cts)
-        if 'J' in counted and cts[0] != 5:
-            cts.remove(counted['J'])
-            cts[0] += counted['J']
-            print('\r added', counted['J'], cts)
-
-        if cts[0] == 5:
-            return 0  # Five of a kind
-        elif cts[0] == 4:
-            return 1  # Four of a kind
-        elif cts[0] == 3:
-            if cts[1] == 2:
-                return 2  # Full house
-            else:
-                return 3  # Three of a kind
-        elif cts[0] == 2:
-            if cts[1] == 2:
-                return 4  # Two pair
-            else:
-                return 5  # One pair
-        else:
-            return 6
-
-    repl = {c: string.ascii_lowercase[i] for i, c in enumerate('AKQT98765432J')}
-    # print(repl)
-    hands = []
-    for line in data:
-        cards, bid = line.split()
-        type = get_type(cards)
-        processed = str(type) + ''.join(repl[k] for k in cards)
+        processed = str(get_type(cards, p2)) + ''.join(repl[k] for k in cards)
         hands.append((processed, cards, int(bid)))
-    hands.sort()
-    hands.reverse()
+    hands.sort(reverse=True)
     acc = 0
     for i, (processed, cards, bid) in enumerate(hands):
-        print(i, processed, cards, bid)
         acc += (i + 1) * bid
     return acc
 
@@ -97,5 +48,5 @@ setday(7)
 
 data = parselines()
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1:', solve(False) )
+print('part2:', solve(True) )
