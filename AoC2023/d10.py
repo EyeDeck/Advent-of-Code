f9b99a23b2d587111e8e21b580565e2cd42cb180
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-10 02:14:41 -0500

    Wash 2023 day 10
    ...as much as I care to, anyway.

--- AoC2023/d10.py
+++ AoC2023/d10.py
@@ -1,6 +1,5 @@
 from aoc import *
 
-
 NORTH = (0, -1)
 SOUTH = (0, 1)
 EAST = (1, 0)
@@ -14,17 +13,18 @@ valids = {
     '7': (SOUTH, WEST),
     'F': (SOUTH, EAST),
     '.': (),
-    'S': (SOUTH,) #, WEST),  # HARDCODED fix later
 }
 
-def get_next(pos, prev):
-    adj = [vadd(pos,c) for c in valids[grid[pos]]]
-    if prev in adj:
-        adj.remove(prev)
-    # print(adj)
-    return adj[0]
+verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
 def p1():
+    def get_next(pos, prev):
+        adj = [vadd(pos, c) for c in valids[grid[pos]]]
+        if prev in adj:
+            adj.remove(prev)
+
+        return adj[0]
+
     start = inverse['S'][0]
     pos = start
     prev = (0,0)
@@ -32,126 +32,55 @@ def p1():
     while pos != start or prev == (0,0):
         path.append(pos)
         pos, prev = get_next(pos, prev), pos
-        # print(pos)
-    # print(path, len(path))
+
     return len(path)//2
-
-
-# def flood_fill(pos, grid, path, seen):
-#     queue = [pos]
-#     splat = []
-#     while queue:
-#         pos = queue.pop()
-#         for dir in DIRS:
-#             nxt = vadd(dir, pos)
-#             if nxt in path:
-#                 continue
-#             if nxt in seen:
-#                 continue
-#             if nxt not in grid:
-#                 print(pos, 'fell off grid', splat)
-#                 return []
-#             queue.append(nxt)
-#             splat.append(nxt)
-#             seen.add(nxt)
-#         # print(queue)
-#     splat.append(pos)
-#     return splat
-
-def flood_fill(pos, grid, path):
-    queue = {pos}
-    splat = []
-    seen = set()
-
-    i = 0
-    while queue:
-        i += 1
-        # print(len(queue))
-
-        if i % 1000 == 0:
-            print_2d(' ', grid, {k: '@' for k in splat}, constrain=(-256, -256, 512, 512))
-
-        pos = queue.pop()
-        for dir in DIRS:
-            nxt = vadd(dir, pos)
-            if nxt in path:
-                continue
-            if nxt in seen:
-                continue
-            if nxt not in grid:
-               continue
-
-            queue.add(nxt)
-            splat.append(nxt)
-            seen.add(nxt)
-    splat.append(pos)
-    return splat
-
-# def p2():
-#     start = inverse['S'][0]
-#     pos = start
-#     prev = (0, 0)
-#     path = []
-#     while pos != start or prev == (0, 0):
-#         path.append(pos)
-#         pos, prev = get_next(pos, prev), pos
-#         # print(pos)
-#     # print(path, len(path))
-#     path = set(path)
-#     seen = set()
-#     print_2d('.', grid, {k:'@' for k in path})
-#     splotch = []
-#     # width = max(grid.keys(), key=lambda x:x[0])[0]
-#     # height = max(grid.keys(), key=lambda x:x[1])[1]
-#     # print(width, height)
-#     # corners = [(0,0), (width,0), (0,height), (width, height)]
-#     to_add = {}
-#     for k in grid:
-#         for dir in DIRS:
-#             nxt = vadd(dir, k)
-#             if nxt not in grid:
-#                 to_add[nxt] = '.'
-#     grid.update(to_add)
-#     splotch = flood_fill((0,0), grid, path, seen)
-#     inv = set(grid.keys())
-#     for c in splotch:
-#         # print(c, '<- c', len(splotch))
-#         if c in inv:
-#             inv.remove(c)
-#     for c in path:
-#         if c in inv:
-#             inv.remove(c)
-#     print()
-#     print_2d('.', grid, {k: '@' for k in inv}, {k: '_' for k in path})
-#     return len(inv)
-
 
 
 def p2():
     def get_next(pos, prev):
         adj = [vadd(c, vadd(pos, c)) for c in valids[scaled_grid[pos]]]
-        # adj = (adj[0] * 2, adj[1] * 2)
         if prev in adj:
             adj.remove(prev)
-        # print(adj)
         return adj[0]
 
+    def flood_fill(pos, grid, path):
+        queue = {pos}
+        splat = set()
+        seen = set()
+
+        while queue:
+            # print_2d(' ', grid, {k: '@' for k in splat}, constrain=(-256, -256, 512, 512))
+
+            pos = queue.pop()
+            for dir in DIRS:
+                nxt = vadd(dir, pos)
+                if nxt in path:
+                    continue
+                if nxt in seen:
+                    continue
+                if nxt not in grid:
+                    continue
+
+                queue.add(nxt)
+                splat.add(nxt)
+                seen.add(nxt)
+        splat.add(pos)
+        return splat
+
+    # upscale our grid by 2x, so we can check in between pipes
     scaled_grid = {}
     for k,v in grid.items():
         scaled_grid[(k[0]*2, k[1]*2)] = v
 
-    print_2d('.', scaled_grid)
-
     start = vmul(inverse['S'][0], (2,2))
     pos = start
     prev = (0,0)
-    path = []
+    path = set()
     while pos != start or prev == (0,0):
-        path.append(pos)
+        path.add(pos)
         pos, prev = get_next(pos, prev), pos
         in_between = ((pos[0] + prev[0]) // 2, (pos[1] + prev[1]) // 2)
-        print(pos, prev, in_between)
-        path.append(in_between)
+        path.add(in_between)
 
     # outset twice
     for i in range(2):
@@ -163,33 +92,38 @@ def p2():
                     to_add[nxt] = '.'
         scaled_grid.update(to_add)
 
-    print_2d(' ', scaled_grid, {k: '@' for k in path}, constrain=(-256, -256, 512, 512))
+    # find everything outside the loop
+    splotch = flood_fill((-1,-1), scaled_grid, path)
 
-    splotch = flood_fill((0,0), scaled_grid, path)
-    inv = set(scaled_grid.keys())
-    for c in splotch:
-        # print(c, '<- c', len(splotch))
-        if c in inv:
-            inv.remove(c)
-    for c in path:
-        if c in inv:
-            inv.remove(c)
-    # print()
-    # print_2d('.', grid, {k: '@' for k in inv}, {k: '_' for k in path})
+    # now remove that, and the path, from all known positions
+    inv = set(scaled_grid.keys()) - splotch - path
 
-    print('INV', inv)
-    print('\n')
+    # finally cut that down to "original" coords
     evens = [c for c in inv if c[0]&1==0 and c[1]&1==0]
-    print('EVENS', evens)
+
+    if verbose:
+        print('enclosed:')
+        print_2d('.', scaled_grid, {k: 'I' for k in inv}, {k: '~' for k in path}, constrain=(-256, -256, 512, 512))
+
+        def downscale(d):
+            return {(k[0]//2, k[1]//2):v for k,v in d.items() if k[0]&1==0 and k[1]&1==0}
+
+        print('\ndownscaled:')
+        print_2d('.', downscale(scaled_grid), downscale({k: 'I' for k in inv}), downscale({k: '~' for k in path}))
 
     return len(evens)
 
 
 setday(10)
 
-# data = parselines()
-# data = parselines(get_ints)
 grid, inverse, unique = parsegrid()
 
-# print('part1:', p1() )
+# find a direction S can go
+for dir in DIRS:
+    s_adj = vadd(dir, inverse['S'][0])
+    if s_adj in grid and vmul((-1,-1), dir) in valids[grid[s_adj]]:
+        valids['S'] = (dir,)
+        break
+
+print('part1:', p1() )
 print('part2:', p2() )
