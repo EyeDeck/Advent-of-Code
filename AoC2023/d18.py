8adc0e0f0245c5a00e16251f1d93b6b52fa3da5b
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-18 02:48:51 -0500

    Wash 2023 day 18
    Farewell, floodfill

--- AoC2023/d18.py
+++ AoC2023/d18.py
@@ -1,94 +1,39 @@
 from aoc import *
 
-dir_map = {
-    'R': (1, 0),
-    'U': (0, -1),
-    'L': (-1, 0),
-    'D': (0, 1),
-}
 
-def p1():
-    grid = {(0,0)}
-
-    pos = (0,0)
-    for line in data:
-        d, n, color = line.split(' ')
-        n = int(n)
-
-        # print(d,n, color)
-        for i in range(n):
-            pos = vadd(dir_map[d], pos)
-            grid.add(pos)
-
-    stack = {(1,1)}
-    while stack:
-        cur = stack.pop()
-        grid.add(cur)
-        for d in DIRS:
-            nxt = vadd(cur, d)
-            if nxt in grid:
-                continue
-            stack.add(nxt)
-
-    # print_2d(' ', {c:'.' for c in grid}, {(0,0):'0'}, constrain=(-1000, -1000, 1000, 1000))
-    return len(grid)
-
-
-def shoelace(p):
-    return abs(sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in segments(p))) // 2
-
-def segments(p):
-    return zip(p, p[1:] + [p[0]])
-
-def p2():
-    perimeter = []
-    vertices = []
+def get_vertices(instructions):
     pos = (0,0)
     perimeter_len = 0
-    for line in data:
-        _, _, ins = line.split(' ')
-        d, n = int(ins[-2]), int(ins[2:-2], 16)
-
-    # pos = (0, 0)
-    # for line in data:
-    #     d, n, color = line.split(' ')
-    #     print(DIRS.index(dir_map[d]))
-    #     d = DIRS.index(dir_map[d])
-    #     n = int(n)
+    vertices = []
+    for d, n in instructions:
+        vertices.append(pos)
+        pos = vadd(vmul((n,n), DIRS[d]), pos)
         perimeter_len += n
-
-        # print(d,n)
-
+    return vertices, perimeter_len
 
 
-        start_pos = pos
-        end_pos = vadd(vmul((n,n), DIRS[d]), pos)
-        perimeter.append((start_pos, end_pos))
-        vertices.append(start_pos)
-        pos = end_pos
-    # print(perimeter)
-    # print(vertices)
+def get_area(v, p):
+    # formula copied from https://stackoverflow.com/a/451482
+    return abs(sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in zip(v, v[1:] + [v[0]]))) // 2  + p//2 + 1
 
-    # area = polygonArea(vertices)
-    # perimeter = sum(abs(n[0]-n[1])-1 for n in vertices)
-    #
-    # x, y = zip(*vertices)
-    # area = area_by_shoelace(x, y)
 
-    print(vertices)
-    area = shoelace(vertices)
-
-    # print_2d('  _   ', {k:k for k in vertices})
-
-    print(area, perimeter_len)
-
-    return area + perimeter_len//2 + 1
+def solve(p2):
+    instructions = []
+    for line in data:
+        if p2:
+            _, _, ins = line.split(' ')
+            d = int(ins[-2])
+            n = int(ins[2:-2], 16)
+        else:
+            d, n, _ = line.split(' ')
+            d = 'RDLU'.index(d)
+            n = int(n)
+        instructions.append((d,n))
+    return get_area(*get_vertices(instructions))
 
 setday(18)
 
 data = parselines()
-# data = parselines(get_ints)
-# grid, inverse, unique = parsegrid()
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1:', solve(False) )
+print('part2:', solve(True) )
