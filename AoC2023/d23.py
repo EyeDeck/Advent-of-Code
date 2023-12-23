a788d73c935e5dc0e6332dae0d9718bd819543e8
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-23 08:59:25 -0500

    Wash 2023 day 23
    Scrub scrub...
    I noticed that my part 1 and part 2 code, although I wrote it somewhat differently for each part, was functionally very similar, and almost all of it could be shared.

--- AoC2023/d23.py
+++ AoC2023/d23.py
@@ -2,58 +2,19 @@ from aoc import *
 import networkx as nx
 
 
-def bfs(src, tgt, neighbors):
-    q = deque([src])
-
-    parent = {}
-
-    while q:
-        cur = q.popleft()
-        if cur == tgt:
-            break
-
-        for n in neighbors[cur]:
-            if n in parent:
-                continue
-            parent[n] = cur
-            q.append(n)
-
-    if tgt not in parent:
-        return None
-
-    pos = tgt
-    path = []
-    while pos != src:
-        path.append(pos)
-        pos = parent[pos]
-    path.append(src)
-    path.reverse()
-    return path
-
-
-def p1():
-    dmap = {
+def define_edges(G, grid, p2):
+    slope_map = {
         (1, 0): '>',
         (0, -1): '^',
         (-1, 0): '<',
         (0, 1): 'v'
     }
 
-    start, end = unique['S'], unique['E']
-    G = nx.DiGraph()
-
-    G.add_node(start)
-    G.add_node(end)
-    for c, tile in grid.items():
-        if tile not in 'v>':
-            continue
-        G.add_node(c)
-
-    def flood(start):
-        q = [vadd(start, ((1,0) if grid[start] == '>' else (0,1)))]
-        parent = {start: None, q[0]: start}
-        ends = []
+    for node in G.nodes():
+        parent = {node: None}
+        q = deque([node])
         while q:
+            # print(q)
             cur = q.pop()
 
             for dir in DIRS:
@@ -68,61 +29,27 @@ def p1():
                 if ntile == '#':
                     continue
 
-                parent[n] = cur
-
-                if ntile == '.':
+                if n in G.nodes():
+                    w = 1
+                    while cur not in G.nodes():
+                        w += 1
+                        cur = parent[cur]
+                    # print(cur, grid[cur], 'connects to', n, grid[n])
+                    G.add_edge(cur, n, weight=w)
+                elif ntile == '.' or (p2 or ntile in 'v>' and ntile == slope_map[dir]):
+                    parent[n] = cur
                     q.append(n)
 
-                if ntile == 'E' or ntile in 'v>' and ntile == dmap[dir]:
-                    ends.append(n)
 
-        weights = {}
-        for c in ends:
-            path = []
-            cur = c
-            while cur != start:
-                path.append(cur)
-                cur = parent[cur]
-            weights[c] = len(path)
-        return weights
-
-    print(flood(start))
-
-    for node in G.nodes():
-        if node == end:
-            continue
-        edges = flood(node)
-        print(node, grid[node], edges)
-        for edge, w in edges.items():
-            G.add_edge(node, edge, weight=w)
-
-    return nx.dag_longest_path_length(G)
-
-
-def p2():
-    start, end = unique['S'], unique['E']
-    G = nx.Graph()
+def find_intersections(grid, start, end, G):
     G.add_node(start)
     G.add_node(end)
-
-    p2_grid = {k:'.' if v in '>v' else v for k,v in grid.items()}
-
-    def backtrack(start):
-        w = 1
-        prev = parent[start]
-        while prev not in G.nodes():
-            w += 1
-            prev = parent[prev]
-
-        G.add_edge(start, prev, weight=w)
-        print('added edge from', start, p2_grid[cur], 'to', prev, p2_grid[prev], 'with weight', w)
 
     intersections = set()
     parent = {start: None}
     q = deque([start])
     while q:
         cur = q.pop()
-        # print(p2_grid[cur])
 
         neighbors = []
         for dir in DIRS:
@@ -130,10 +57,10 @@ def p2():
             if n in parent:
                 continue
 
-            if n not in p2_grid:
+            if n not in grid:
                 continue
 
-            ntile = p2_grid[n]
+            ntile = grid[n]
             if ntile == '#':
                 continue
 
@@ -141,68 +68,38 @@ def p2():
             neighbors.append(n)
 
         if len(neighbors) > 1:
-            # print(cur, neighbors)
             G.add_node(cur)
             intersections.add(cur)
 
-            # backtrack(cur)
-
         for n in neighbors:
             q.append(n)
+    return G
 
-    # backtrack(end)
+def solve(p2):
+    open = {k for k,v in grid.items() if v == '.'}
+    start, end = min(open, key=itemgetter(1)), max(open, key=itemgetter(1))
 
-    print_2d(' ', grid, {k:'+' for k in intersections})
-    print_2d('         ', {k:f'+{k}' for k in intersections})
+    G = find_intersections(grid, start, end, nx.Graph() if p2 else nx.DiGraph())
 
-    for intersection in intersections:
-        parent = {intersection: None}
-        q = deque([intersection])
-        while q:
-            cur = q.pop()
+    define_edges(G, grid, p2)
 
-            neighbors = []
-            for dir in DIRS:
-                n = vadd(dir, cur)
-                if n in parent:
-                    continue
-
-                if n not in p2_grid:
-                    continue
-
-                ntile = p2_grid[n]
-                if ntile == '#':
-                    continue
-
-                if n in intersections or ntile in 'SE':
-                    w = 1
-                    while cur not in intersections:
-                        w += 1
-                        cur = parent[cur]
-                    print(cur, p2_grid[cur], 'connects to', n, p2_grid[n])
-                    G.add_edge(cur, n, weight=w)
-                    # do the stuff
-                else:
-                    parent[n] = cur
-                    q.append(n)
-
-        print(intersection)
-
-    longest = 0
-    for path in nx.all_simple_paths(G, start, end):
-        # print(path)
-        l = nx.path_weight(G, path, 'weight')
-        if l > longest:
-            longest = l
-            print(longest)
-
-    return longest
+    if p2:
+        longest = 0
+        for path in nx.all_simple_paths(G, start, end):
+            # print(path)
+            l = nx.path_weight(G, path, 'weight')
+            if l > longest:
+                longest = l
+                print(longest, end='...\r')
+        return longest
+    else:
+        return nx.dag_longest_path_length(G)
 
 
 setday(23)
 
 grid, inverse, unique = parsegrid()
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1:', solve(False) )
+print('part2:', solve(True) )
 
