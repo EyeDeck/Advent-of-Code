da1244ae70a6d506fbf681904baeef31dea4e1d6
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-12-25 00:33:07 -0500

    Wash 2023 day 25
    Doesn't get much cleaner than this.

--- AoC2023/d25.py
+++ AoC2023/d25.py
@@ -1,31 +1,20 @@
-import random
-from aoc import *
+import math
 
+from aoc import *
 import networkx as nx
-from networkx.algorithms.connectivity import minimum_st_edge_cut
 
 def p1():
     G = nx.Graph()
 
     for line in data:
         a, b = line.split(': ')
-        for x in b.strip().split():
-            G.add_node(a)
-            G.add_node(x)
-            G.add_edge(a, x)
+        for c in b.strip().split():
+            G.add_edge(a, c)
 
-    while True:
-        a, b = random.choice([*G.nodes()]), random.choice([*G.nodes()])
-        cuts = minimum_st_edge_cut(G, a, b)
-        if len(cuts) > 3:
-            continue
-        for cut in cuts:
-            G.remove_edge(*cut)
+    for cut in nx.minimum_edge_cut(G):
+        G.remove_edge(*cut)
 
-        return len(nx.node_connected_component(G, a)) * len(nx.node_connected_component(G, b))
-
-    return None
-
+    return math.prod(len(c) for c in nx.connected_components(G))
 
 setday(25)
 
