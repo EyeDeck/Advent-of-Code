0f0e50e72c494daa0dd67b9ee5b68aa58c0e95f0
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-01-04 13:48:31 -0500

    Solve 2016 day 11
    61933:15:?? (6523) / 61933:33:?? (6171)
    
    This one's bugged me for years...I got filtered by it the first time I tried participating in AoC back in 2016. In fact, I didn't even attempt to solve it that year, I just looked at the description + input and gave up.
    
    My "solution" is awful, I started with a basic BFS, which slowly evolved into Dijkstra when I tried prioritizing more likely looking paths, and at this point I think it has morphed into A* with a barley-admissible heuristic. Oh, and it takes about 11.5 minutes + 20GB of RAM for part 2. I'm just happy it gets there at all, I didn't have to modify my part 1 solution beyond adding the extra objects, it just took some patience (read: a lot of self-control to not ctrl+C after 30 seconds).

--- AoC2016/d11.py
+++ AoC2016/d11.py
@@ -4,7 +4,10 @@ from aoc import *
 
 
 
-def p1():
+def solve(p2):
+    if p2:
+        data[0] += 'elerium generator, elerium-compatible microchip, dilithium generator, dilithium-compatible microchip'
+
     floors = {}
     for i, line in enumerate(data):
         floors[i] = defaultdict(list)
@@ -17,111 +20,134 @@ def p1():
                 floors[i]['gens'].append(l)
     print(floors)
 
-    state = copy.deepcopy(floors)
-    state['floor'] = 0
-    state['min'] = 0
+    cur = copy.deepcopy(floors)
+    cur['floor'] = 0
+    cur['min'] = 0
 
     q = []
-    heapq.heappush(q, [0, 0, 0, state])
+    heapq.heappush(q, [0, 0, 0, cur])
     print(q)
 
     seen = {}
+    parent = {0: [None, cur]}
 
     i = 0
     while q:
         if i % 10000 == 0:
             print(len(seen), len(q))
         # steps, cur = q.popleft()
-        score, steps, _, cur = heapq.heappop(q)
-        # print('popped', steps, cur)
+        score, steps, q_id, cur = heapq.heappop(q)
+        # print(f'popped {score}, {steps}, {q_id}, \n{cur}')
 
         if cur['min'] == 3:
             print(steps, cur)
+
+            thing = q_id
+            while thing is not None:
+                thing, node = parent[thing]
+                print(node, thing)
+
             return steps
 
         neighbors = []
 
-        floor = cur['floor']
+        def is_valid(state, floors_to_check):
+            # if len(next_state[next_floor]['gens']) > 0:
+            #     invalid = False
+            #     for chip in next_state[next_floor]['chips']:
+            #         if chip not in next_state[next_floor]['gens']:
+            #             invalid = True
+            #             break
+            #     if invalid:
+            #         continue
 
-        adj_floors = [f for f in [floor-1, floor+1] if cur['min'] <= f <= 3]
+            # print('input state:\n', state)
+            for f in floors_to_check:
+                f_gens = state[f]['gens']
+                f_chips = state[f]['chips']
+                for chip in f_chips:
+                    if chip in f_gens:
+                        continue
+                    elif len(f_gens) > 0:
+                        # print('invalid', state)
+                        # input()
+                        return False
+                # f_gens = state[f]['gens'].copy()
+                # f_chips = state[f]['chips'].copy()
+                # # print(f_gens, f_chips, '/', next_state[f]['gens'], next_state[f]['chips'])
+                # for chip in f_chips.copy():
+                #     if chip in f_gens:
+                #         f_chips.remove(chip)
+                #         f_gens.remove(chip)
+                # # print(f_gens, f_chips, '/', next_state[f]['gens'], next_state[f]['chips'])
+                # if len(f_chips) > 0 and len(f_gens) > 0:
+                #     # print('invalid? on floor', f, '\n', state, '\n', '->', f_gens, f_chips)
+                #     return False
+                #     # input()
+            # print('valid?\n')
+            # print(next_state[floor]['gens'], next_state[floor]['chips'], '/', f_gens, f_chips)
+            # input()
+            # print('valid', state)
+            return True
 
-        # print(floor, steps, adj_floors)
+        def get_neighbors(state):
+            cur_floor = state['floor']
 
-        cur_gens = cur[floor]['gens']
-        cur_chips = cur[floor]['chips']
+            adj_floors = [f for f in [cur_floor - 1, cur_floor + 1] if state['min'] <= f <= 3]
 
-        combs = [*itertools.combinations([('gen', g) for g in cur_gens] + [('chip', c) for c in cur_chips] + [None], 2)]
-        # print(combs)
+            # print(floor, steps, adj_floors)
+
+            cur_gens = state[cur_floor]['gens']
+            cur_chips = state[cur_floor]['chips']
+
+            combs = [
+                *itertools.combinations([('gen', g) for g in cur_gens] + [('chip', c) for c in cur_chips] + [None], 2)]
+            # print(combs)
+
+            for next_floor in adj_floors:
+                for things_to_take in combs:
+                    # if next_floor > cur_floor and None in things_to_take:
+                    #     continue
+
+                    next_state = copy.deepcopy(state)
+                    next_state['floor'] = next_floor
+
+                    for thing in things_to_take:
+                        if thing is None:
+                            continue
+                        elif thing[0] == 'chip':
+                            next_state[cur_floor]['chips'].remove(thing[1])
+                            next_state[next_floor]['chips'].append(thing[1])
+                            next_state[next_floor]['chips'].sort()
+                        elif thing[0] == 'gen':
+                            next_state[cur_floor]['gens'].remove(thing[1])
+                            next_state[next_floor]['gens'].append(thing[1])
+                            next_state[next_floor]['gens'].sort()
+
+                    if not is_valid(next_state, [cur_floor, next_floor]):
+                        continue
+                    # input()
+
+                    yield next_state
 
         next_steps = steps + 1
 
-        for next_floor in adj_floors:
+        for next_state in get_neighbors(cur):
+            lowest_floor = next_state[next_state['min']]
+            if len(lowest_floor['gens']) == 0 and len(lowest_floor['chips']) == 0:
+                # print('inc lowest...')
+                del next_state[next_state['min']]
+                next_state['min'] += 1
+                # print(next_state)
 
-            for things_to_take in combs:
+            hashable = str(next_state)
+            if hashable in seen and seen[hashable] <= next_steps:
+                # print(seen[hashable], next_steps, 'found better path to', hashable)
+                continue
 
-                # if next_floor < adj_floors and None in things_to_take:
-                #     continue
+            seen[hashable] = next_steps
 
-                next_state = copy.deepcopy(cur)
-                next_state['floor'] = next_floor
-
-                for thing in things_to_take:
-                    if thing is None:
-                        continue
-                    elif thing[0] == 'chip':
-                        next_state[floor]['chips'].remove(thing[1])
-                        next_state[next_floor]['chips'].append(thing[1])
-                        next_state[next_floor]['chips'].sort()
-                    elif thing[0] == 'gen':
-                        next_state[floor]['gens'].remove(thing[1])
-                        next_state[next_floor]['gens'].append(thing[1])
-                        next_state[next_floor]['gens'].sort()
-
-                # if len(next_state[next_floor]['gens']) > 0:
-                #     invalid = False
-                #     for chip in next_state[next_floor]['chips']:
-                #         if chip not in next_state[next_floor]['gens']:
-                #             invalid = True
-                #             break
-                #     if invalid:
-                #         continue
-
-                valid = True
-                for f in [floor, next_floor]:
-                    f_gens = next_state[f]['gens'].copy()
-                    f_chips = next_state[f]['chips'].copy()
-                    # print(f_gens, f_chips, '/', next_state[f]['gens'], next_state[f]['chips'])
-                    for chip in f_chips.copy():
-                        if chip in f_gens:
-                            f_chips.remove(chip)
-                            f_gens.remove(chip)
-                    # print(f_gens, f_chips, '/', next_state[f]['gens'], next_state[f]['chips'])
-                    if len(f_chips) > 0 and len(f_gens) > 0:
-                        # print('invalid? on floor', f, '\n', next_state, '\n', '->', f_gens, f_chips)
-                        valid = False
-                        # input()
-                        break
-                if not valid:
-                    continue
-                # print('valid?', '\n', next_state, '\n')
-                    # print(next_state[floor]['gens'], next_state[floor]['chips'], '/', f_gens, f_chips)
-                    # input()
-
-                lowest_floor = next_state[next_state['min']]
-                if len(lowest_floor['gens']) == 0 and len(lowest_floor['chips']) == 0:
-                    # print('inc lowest...')
-                    del next_state[next_state['min']]
-                    next_state['min'] += 1
-                    # print(next_state)
-
-                hashable = str(next_state)
-                if hashable in seen and seen[hashable] <= next_steps:
-                    # print(seen[hashable], next_steps, 'found better path to', hashable)
-                    continue
-
-                seen[hashable] = next_steps
-
-                neighbors.append(next_state)
+            neighbors.append(next_state)
 
         # print('\nneighbors:')
         for n in neighbors:
@@ -131,20 +157,15 @@ def p1():
                 # print(floor, n[floor]['gens'], n[floor]['chips'], (3-floor) * (len(n[floor]['gens']) + len(n[floor]['chips'])))
                 score += (3-floor) * (len(n[floor]['gens']) + len(n[floor]['chips']))
 
-            # print(n)
-            # input()
-            # print(score)
-            # print('added', n)
-            # input()
-            heapq.heappush(q, [next_steps * score, next_steps, i, n])
+            # trial and error heuristic, I don't really know why it works at all
+            heapq.heappush(q, [(next_steps-5) * score, next_steps, i, n])
+
+            parent[i] = [q_id, n]
             # print(q[-1])
 
         # input()
 
     return None
-
-    # 29 not right...
-    # 27 not right
 
 
 def p2():
@@ -155,5 +176,5 @@ setday(11)
 
 data = parselines()
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1:', solve(False) )
+print('part2:', solve(True) )
