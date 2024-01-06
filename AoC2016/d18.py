4dfd017184f1cf3839ed1ce8232d9423c9e74462
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-01-06 17:10:07 -0500

    Poke 2016 day 18
    I can confirm experimentally that there is, in fact, no cycling within the given 400k lines. You just have to do the math, and Python is Python, which is to say slow.
    Via pigeonhole principle we can deduce that a cycle must start at some point within the first 2^100 lines, however.

--- AoC2016/d18.py
+++ AoC2016/d18.py
@@ -5,11 +5,19 @@ def solve(h):
     w = max(grid.keys(), key=itemgetter(0))[0] + 1
     traps = set(inverse['^'])
     adj = [(-1,-1), (0,-1), (1,-1)]
+    seen = set()
     for y in range(h):
+        row = set()
         for x in range(w):
             a,b,c = [vadd((x,y), d) in traps for d in adj]
             if (a and b and not c) or (not a and b and c) or (a and not b and not c) or (not a and not b and c):
-                traps.add((x,y))
+                row.add((x,y))
+        traps |= row
+        row = frozenset(x for x,y in row)
+        if row in seen:
+            print('cycling at row', y)
+            break
+        seen.add(row)
     # print_2d('.', {k:'^' for k in traps})
     return (h*w) - len(traps)
 
