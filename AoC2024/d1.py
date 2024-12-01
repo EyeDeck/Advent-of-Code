10072cac0f7f168a4d9b5a1858412ed6bda5f919
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-01 01:02:41 -0500

    Wash 2024 day 1

--- AoC2024/d1.py
+++ AoC2024/d1.py
@@ -1,61 +1,26 @@
-import sys
-import re
-# import numpy as np
-from collections import defaultdict  # defaultdict(int)
-import functools  # @functools.cache
-from collections import *
-from math import *
-from pprint import pprint
-
+from collections import Counter
 from aoc import *
 
 
-# print_2d(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# print_2d_repl(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# INF = sys.maxsize
-# @memo
-# splat(f)
-# bfs(src, tgt, {n1: [n2, n3, n4]})
-# wbfs(src, tgt, edge_func)
-# astar(src, tgt, edge_func, heur_func)
-# ccs(srcs, neigh_func, filt_func?)
-# Dedup().seen(x)
-
-
 def p1():
-    a, b = [], []
-    for line in data:
-        x, y = [int(i) for i in line.split()]
-        a.append(x)
-        b.append(y)
-    a.sort()
-    b.sort()
-    acc = 0
-    for x,y in zip(a,b):
-        acc += abs(x-y)
-    return acc
+    return sum(abs(x - y) for x, y in zip(a, b))
 
 
 def p2():
-    a, b = [], []
-    for line in data:
-        x, y = [int(i) for i in line.split()]
-        a.append(x)
-        b.append(y)
     rlist = Counter(b)
-    acc = 0
-    for n in a:
-        acc += n * rlist[n]
-    return acc
+    return sum(n * rlist[n] for n in a)
 
 
 setday(1)
 
 data = parselines()
-# parsed = [[int(i) for i in line.split()] for line in data]
-# print(parsed)
-# data = parselines(get_ints)
-# grid, inverse, unique = parsegrid()
+a, b = [], []
+for line in data:
+    x, y = [int(i) for i in line.split()]
+    a.append(x)
+    b.append(y)
+a.sort()
+b.sort()
 
 print('part1:', p1() )
 print('part2:', p2() )
