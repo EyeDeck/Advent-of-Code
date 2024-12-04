711e87e985ca387c1ad283f56c5e7be00945f653
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-04 01:21:52 -0500

    Wash 2024 day 4
    Cleaned up, and added -v / --verbose flag to print match grid like the puzzle description.

--- AoC2024/d4.py
+++ AoC2024/d4.py
@@ -3,34 +3,36 @@ from aoc import *
 
 def p1():
     acc = 0
-    print_2d(' ', grid)
+    marked_grid = set()
+
     tgt = 'XMAS'
     for coord, char in grid.items():
         if char != 'X':
             continue
 
         for dir in DIAGDIRS:
+            clist = {coord}
             for i, char in enumerate(tgt):
                 n_c = vadd(coord, vmul(dir, i))
-                # print(coord, dir, i, n_c)
                 if n_c not in grid:
                     break
-                # print(grid[n_c])
                 if grid[n_c] != char:
                     break
+                clist.add(n_c)
             else:
-                # print('yes')
                 acc += 1
 
-            # print(dir)
+                marked_grid |= clist
+
+    if verbose:
+        print_2d('.', {(0, 0): '.', max(grid.keys()): '.'} | {k: grid[k] for k in marked_grid})
     return acc
 
 
 def p2():
     acc = 0
-    marked_grid = {}
+    marked_grid = set()
 
-    orth = DIAGDIRS[::2]
     diag = DIAGDIRS[1::2]
 
     for coord, char in grid.items():
@@ -38,27 +40,27 @@ def p2():
             continue
 
         try:
-            crosses = [{k: grid[k] for k in [vadd(coord, c) for c in coordset]} for coordset in [diag]]
-            print(crosses)
+            cross = {k: grid[k] for k in [vadd(coord, c) for c in diag]}
         except KeyError:
             continue
 
-        for cross in crosses:
-            cross_vals = [k for k in cross.values()]
-            if cross_vals.count('M') == 2 and cross_vals.count('S') == 2 and cross_vals[0] != cross_vals[2]:
-                acc += 1
+        cross_vals = [k for k in cross.values()]
+        if cross_vals.count('M') == 2 and cross_vals.count('S') == 2 and cross_vals[0] != cross_vals[2]:
+            acc += 1
 
-                marked_grid |= cross
-                marked_grid[coord] = 'A'
+            marked_grid |= cross.keys()
+            marked_grid.add(coord)
 
-        # print(orth_chars, diag_chars)
-    print_2d('.', marked_grid)
+    if verbose:
+        print_2d('.', {(0, 0): '.', max(grid.keys()): '.'} | {k: grid[k] for k in marked_grid})
     return acc
 
 
 setday(4)
 
 grid, inverse, unique = parsegrid()
+
+verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
 print('part1:', p1())
 print('part2:', p2())
