323b7f1ed4e940779c079e5d16b0e0d6a82274aa
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-05 02:25:55 -0500

    Optimize 2024 day 5
    Replaced the bubblesort or whatever it was I wrote with Python's standard sort (probably Powersort or Timsort, I guess), which also significantly reduces code complexity.
    I wish I could eliminate the cmp_to_key, but I can't for the life of me think of how to write that part the Python 3 way.

--- AoC2024/d5.py
+++ AoC2024/d5.py
@@ -1,35 +1,28 @@
+import functools
+
 from aoc import *
 
 
-def verify(pageset, rules_before):
-    for i, page in enumerate(pageset):
-        pages_after = pageset[i + 1:]
-        for j, page_after in enumerate(pages_after):
-            if page_after in rules_before[page]:
-                pageset[i+j+1], pageset[i] = pageset[i], pageset[i+j+1]
-                return False
-    return pageset[len(pageset) // 2]
+def compare(a, b):
+    if a in rules_before[b]:
+        return -1
+    elif a in rules_after[b]:
+        return 1
+    else:
+        return 0
 
 
 def solve():
-    # set must go BEFORE key
-    rules_before = defaultdict(set)
-
-    for rule in rules_raw:
-        rules_before[rule[1]].add(rule[0])
-
     acc_p1 = 0
     acc_p2 = 0
     for pageset in pagesets_raw:
-        r = verify(pageset, rules_before)
-        if r:
-            acc_p1 += r
-            continue
-        while True:
-            r = verify(pageset, rules_before)
-            if r:
-                acc_p2 += r
-                break
+        sorted_pageset = sorted(pageset, key=functools.cmp_to_key(compare))
+        middle = sorted_pageset[len(sorted_pageset) // 2]
+
+        if sorted_pageset == pageset:
+            acc_p1 += middle
+        else:
+            acc_p2 += middle
 
     return acc_p1, acc_p2
 
@@ -39,8 +32,13 @@ setday(5)
 with open_default() as file:
     rules_raw, pagesets_raw = file.read().split('\n\n')
 
-rules_raw = [[int(i) for i in line.split('|')] for line in rules_raw.split('\n')]
-pagesets_raw = [[int(i) for i in line.split(',')] for line in pagesets_raw.split('\n')]
+pagesets_raw = [[int(i) for i in line.split(',')] for line in pagesets_raw.strip().split('\n')]
+rules_raw = [[int(i) for i in line.split('|')] for line in rules_raw.strip().split('\n')]
 
+rules_before = defaultdict(set)
+rules_after = defaultdict(set)
+for rule in rules_raw:
+    rules_before[rule[1]].add(rule[0])
+    rules_after[rule[0]].add(rule[1])
 
 print('part1: %d\npart2: %d' % solve())
