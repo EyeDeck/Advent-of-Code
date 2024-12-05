ecbc16f2652bb1228352b98cbcc62b7898941b85
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-05 00:51:54 -0500

    Wash 2024 day 5
    A little better.

--- AoC2024/d5.py
+++ AoC2024/d5.py
@@ -1,70 +1,37 @@
 from aoc import *
 
 
-def verify(pageset, rules_before, rules_after, fix=False):
-    print(pageset)
+def verify(pageset, rules_before):
     for i, page in enumerate(pageset):
-        pages_before = pageset[:i]
         pages_after = pageset[i + 1:]
-        for j, page_before in enumerate(pages_before):
-            if page_before in rules_after[page]:
-                print('a', i,i+j+1)
-                if fix:
-                    pageset[i+j+1], pageset[i] = pageset[i], pageset[i+j+1]
-                return False
         for j, page_after in enumerate(pages_after):
             if page_after in rules_before[page]:
-                print('b', i,i+j+1)
-                if fix:
-                    pageset[i+j+1], pageset[i] = pageset[i], pageset[i+j+1]
+                pageset[i+j+1], pageset[i] = pageset[i], pageset[i+j+1]
                 return False
-        print(page, pages_before, pages_after)
     return pageset[len(pageset) // 2]
 
 
-def p1():
+def solve():
     # set must go BEFORE key
     rules_before = defaultdict(set)
-    # set must go AFTER key
-    rules_after = defaultdict(set)
 
     for rule in rules_raw:
         rules_before[rule[1]].add(rule[0])
-        rules_after[rule[0]].add(rule[1])
-    print(rules_before)
-    print(rules_after)
 
-    acc = sum(verify(pageset, rules_before, rules_after) for pageset in pagesets_raw)
-    return acc
-
-
-def p2():
-    # set must go BEFORE key
-    rules_before = defaultdict(set)
-    # set must go AFTER key
-    rules_after = defaultdict(set)
-
-    for rule in rules_raw:
-        rules_before[rule[1]].add(rule[0])
-        rules_after[rule[0]].add(rule[1])
-    print(rules_before)
-    print(rules_after)
-
-    bad_pagesets = []
+    acc_p1 = 0
+    acc_p2 = 0
     for pageset in pagesets_raw:
-        if not verify(pageset, rules_before, rules_after, True):
-            bad_pagesets.append(pageset)
-    print(bad_pagesets)
+        r = verify(pageset, rules_before)
+        if r:
+            acc_p1 += r
+            continue
+        while True:
+            r = verify(pageset, rules_before)
+            if r:
+                acc_p2 += r
+                break
 
-    fixed_pagesets = []
-    for pageset in bad_pagesets:
-        while not verify(pageset, rules_before, rules_after, True):
-            pass
-        fixed_pagesets.append(pageset)
-        print('fixed:', pageset)
-
-    acc = sum(verify(pageset, rules_before, rules_after) for pageset in fixed_pagesets)
-    return acc
+    return acc_p1, acc_p2
 
 
 setday(5)
@@ -76,5 +43,4 @@ rules_raw = [[int(i) for i in line.split('|')] for line in rules_raw.split('\n')
 pagesets_raw = [[int(i) for i in line.split(',')] for line in pagesets_raw.split('\n')]
 
 
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1: %d\npart2: %d' % solve())
