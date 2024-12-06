006a00a2fa90f0780d7d7ea45cc902b51b4ec7bb
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-06 01:23:24 -0500

    Optimize 2024 day 6
    Added obvious major optimization: only check barricade placement points that were in the unmodified path of the guard (i.e. part 1). Cuts runtime by a further 80% or so, down to about 15s.

--- AoC2024/d6.py
+++ AoC2024/d6.py
@@ -15,7 +15,7 @@ def simulate(sim_grid, extra, pos, heading):
         next_tile = vadd(pos, dir_map[heading])
         if next_tile not in sim_grid:
             sim_grid.update(old)
-            return len(visited_tiles)
+            return len(visited_tiles), visited_tiles
         elif sim_grid[next_tile] == '#':
             heading = dir_next[heading]
         else:
@@ -23,7 +23,7 @@ def simulate(sim_grid, extra, pos, heading):
             to_add = (heading, pos)
             if to_add in visited_states:
                 sim_grid.update(old)
-                return math.inf
+                return math.inf, visited_tiles
             visited_states.add(to_add)
             visited_tiles.add(pos)
 
@@ -34,13 +34,11 @@ def p1():
 
 def p2():
     acc = 0
-    total_points = len(grid.keys())
-    for i, (grid_point, grid_thing) in enumerate(grid.items()):
+    total_points = len(p1_visited)
+    for i, grid_point in enumerate(p1_visited):
         if i % 10 == 0:
             print(f'part2: {acc} ({(i / total_points) * 100:.2f}%)', end='\r')
-        if grid_thing != '.':
-            continue
-        r = simulate(grid, {unique['^']: '.', grid_point: '#'}, unique['^'], '^')
+        r, _ = simulate(grid, {unique['^']: '.', grid_point: '#'}, unique['^'], '^')
         if r == math.inf:
             acc += 1
     sys.stdout.write('\x1b[2K')  # ANSI erase line
@@ -54,5 +52,8 @@ grid, inverse, unique = parsegrid()
 dir_map = {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0)}
 dir_next = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
 
-print('part1:', p1())
+p1_result, p1_visited = p1()
+p1_visited.remove(unique['^'])
+
+print('part1:', p1_result)
 print('part2:', p2())
