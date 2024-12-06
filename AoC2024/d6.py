4f4dbf071656d7648d1a9cfa6c863f09b7d250be
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-06 01:04:17 -0500

    Wash 2024 day 6
    Still takes slightly over a minute, but now cleaner and with a progress indicator!

--- AoC2024/d6.py
+++ AoC2024/d6.py
@@ -1,68 +1,58 @@
-import copy
+import math
 from aoc import *
 
 
-def p1():
-    dir_map = {'^': (0,-1), '>': (1,0), 'v':(0,1), '<':(-1,0)}
-    dir_next = {'^':'>', '>':'v', 'v':'<', '<':'^'}
-    p1_grid = copy.deepcopy(grid)
-    pos = unique['^']
-    heading = '^'
-    p1_grid[pos] = '.'
-    visited = {pos}
+def simulate(sim_grid, extra, pos, heading):
+    old = {}
+    for k, v in extra.items():
+        old[k] = sim_grid[k]
+        sim_grid[k] = v
+
+    visited_states = {(heading, pos)}
+    visited_tiles = {pos}
+
     while True:
         next_tile = vadd(pos, dir_map[heading])
-        if next_tile not in grid:
-            return len(visited)
-        elif grid[next_tile] == '#':
+        if next_tile not in sim_grid:
+            sim_grid.update(old)
+            return len(visited_tiles)
+        elif sim_grid[next_tile] == '#':
             heading = dir_next[heading]
         else:
             pos = next_tile
-            visited.add(pos)
+            to_add = (heading, pos)
+            if to_add in visited_states:
+                sim_grid.update(old)
+                return math.inf
+            visited_states.add(to_add)
+            visited_tiles.add(pos)
 
-    return None
+
+def p1():
+    return simulate(grid, {unique['^']: '.'}, unique['^'], '^')
 
 
 def p2():
-    dir_map = {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0)}
-    dir_next = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
-
     acc = 0
-    for grid_point, grid_thing in grid.items():
-        print(grid_point, acc)
+    total_points = len(grid.keys())
+    for i, (grid_point, grid_thing) in enumerate(grid.items()):
+        if i % 10 == 0:
+            print(f'part2: {acc} ({(i / total_points) * 100:.2f}%)', end='\r')
         if grid_thing != '.':
             continue
-
-        p2_grid = copy.deepcopy(grid)
-        p2_grid[grid_point] = '#'
-        pos = unique['^']
-        heading = '^'
-        p2_grid[pos] = '.'
-        visited = {(heading, pos)}
-
-        while True:
-            next_tile = vadd(pos, dir_map[heading])
-            if next_tile not in p2_grid:
-                break
-            elif p2_grid[next_tile] == '#':
-                heading = dir_next[heading]
-            else:
-                pos = next_tile
-                to_add = (heading, pos)
-                if to_add in visited:
-                    print('loop!')
-                    acc += 1
-                    break
-                visited.add(to_add)
-                # print(visited)
-
+        r = simulate(grid, {unique['^']: '.', grid_point: '#'}, unique['^'], '^')
+        if r == math.inf:
+            acc += 1
+    sys.stdout.write('\x1b[2K')  # ANSI erase line
     return acc
 
 
 setday(6)
 
-
 grid, inverse, unique = parsegrid()
 
-print('part1:', p1() )
-print('part2:', p2() )
+dir_map = {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0)}
+dir_next = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
+
+print('part1:', p1())
+print('part2:', p2())
