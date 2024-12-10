52337abb092e2bc74064bae246552ff5e30f11ce
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-10 04:31:58 -0500

    Wash 2024 day 9
    My original attempt at solving part 2 was with a really flawed (but fast!) algorithm, which I ended up fixing with some duct tape that unfortunately introduced a nasty O(n^2) complexity bit. I was going over the code in my head and realized I could gain back the speed I was going for by just changing a set into a priority queue! Duh.
    
    Not very important for the real input (~700ms -> 60ms), but matters a LOT for the bigboy (~30m | 1,800,000ms -> 1000ms CPython, or 400ms pypy).

--- AoC2024/d9.py
+++ AoC2024/d9.py
@@ -2,40 +2,40 @@ from aoc import *
 
 
 def p1():
-    blockmap = []
+    block_map = []
     state = True
     id = 0
     for char in data:
         i = int(char)
         if state:
             for _ in range(i):
-                blockmap.append(id)
+                block_map.append(id)
             id += 1
         else:
             for _ in range(i):
-                blockmap.append(None)
+                block_map.append(None)
         state = not state
-    # print(blockmap)
+
     l = 0
     while True:
         try:
-            while blockmap[l] != None:
+            while block_map[l] is not None:
                 l += 1
         except IndexError:
             break
-        # print(l, len(blockmap))
-        block = blockmap.pop()
-        if block == None:
+
+        block = block_map.pop()
+        if block is None:
             continue
-        blockmap[l] = block
-    # print(blockmap)
-    return sum(i * n for i, n in enumerate(blockmap))
+        block_map[l] = block
+
+    return sum(i * n for i, n in enumerate(block_map))
 
 
 def p2():
     meta_filled = {}
-    # meta_free = {}
-    free_bins = defaultdict(set)
+
+    free_bins = defaultdict(list)
     state = True
     id = 0
     pos = 0
@@ -44,49 +44,41 @@ def p2():
         if state:
             meta_filled[id] = (pos, i)
             pos += i
-            # for _ in range(i):
-            #     map.append(id)
         else:
-            # meta_free[id] = [len(map), i]
             if i != 0:
-                free_bins[i].add(pos)
+                free_bins[i].append(pos)
             pos += i
-            # for _ in range(i):
-            #     map.append(None)
             id += 1
         state = not state
 
-    # print(meta_filled)
-    # print(free_bins)
-
-    def get_best_bin(bins):
-        lowest = INF
-        best_bin = -1
-        for bin in bins:
-            lowest_in_bin = min(free_bins[bin])
-            if lowest_in_bin < lowest:
-                best_bin = bin
-                lowest = lowest_in_bin
-        return best_bin, lowest
+    for bin in free_bins.values():
+        heapq.heapify(bin)
 
     for id, (position, length) in reversed(meta_filled.items()):
-        compatible_bins = [k for k in free_bins.keys() if k >= length]
-        best_bin, lowest_in_best_bin = get_best_bin(compatible_bins)
+
+        lowest_in_best_bin = INF
+        best_bin = -1
+        for bin_length, current_bin in free_bins.items():
+            if bin_length < length:
+                continue
+            lowest_in_bin = current_bin[0]
+            if lowest_in_bin < lowest_in_best_bin:
+                best_bin = bin_length
+                lowest_in_best_bin = lowest_in_bin
+
         if lowest_in_best_bin == INF or lowest_in_best_bin > position:
             continue
-        leftover = best_bin - length
-        free_bins[best_bin].remove(lowest_in_best_bin)
+
+        heapq.heappop(free_bins[best_bin])
+
         if len(free_bins[best_bin]) == 0:
-            # print('deleted bin', best_bin)
             del free_bins[best_bin]
         meta_filled[id] = (lowest_in_best_bin, length)
-        # print(f'id {id} position {position} length {length} leftover {leftover} moved to {lowest_in_best_bin}')
-        if leftover > 0:
-            free_bins[leftover].add(lowest_in_best_bin + length)
-            # print(f'added leftover {leftover} at pos {lowest_in_best_bin + length}')
 
-    # print(meta_filled)
-    # print(free_bins)
+        leftover = best_bin - length
+        if leftover > 0:
+            heapq.heappush(free_bins[leftover], lowest_in_best_bin + length)
+
     acc = 0
     for k, v in meta_filled.items():
         for n in range(v[0], v[0] + v[1]):
