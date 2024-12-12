f3da7505ab959ab53426cf1bec62c94bbc5a8098
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-12 01:49:02 -0500

    Wash 2024 day 12
    Still a little ugly, but it's fine.

--- AoC2024/d12.py
+++ AoC2024/d12.py
@@ -2,128 +2,74 @@ import copy
 from aoc import *
 
 
-def p1():
+def solve():
+    # separate each unique plot by picking a random tile and flood filling
     unprocessed = copy.deepcopy(grid)
     plots_with_id = {}
     id = 0
     while unprocessed:
-        # print(unprocessed)
-        k,v = unprocessed.popitem()
-        flooded = set()
+        k, v = unprocessed.popitem()
+        flooded = {k}
         frontier = deque([k])
         while frontier:
             point = frontier.popleft()
             if point in unprocessed:
                 del unprocessed[point]
-            flooded.add(point)
             neighbors = [vadd(point, dir) for dir in DIRS]
             for neighbor in neighbors:
-                if neighbor not in grid or neighbor in flooded or neighbor in frontier:
+                if neighbor not in grid or neighbor in flooded:
                     continue
                 if grid[neighbor] == v:
                     frontier.append(neighbor)
+                    flooded.add(neighbor)
         plots_with_id[id] = flooded
         id += 1
-    print(plots_with_id)
 
+    # remake a grid, where value is a unique plot ID instead of plant type
     grid_separated = {}
-    for k,v in plots_with_id.items():
+    for k, v in plots_with_id.items():
         for point in v:
             grid_separated[point] = k
 
-    print(grid_separated)
-
-    areas = {k:len(v) for k,v in plots_with_id.items()}
-    perimeters = defaultdict(int)
-    for plot, points in plots_with_id.items():
-        print(plot, points)
-        for point in points:
-            neighbors = [vadd(point, dir) for dir in DIRS]
-            for neighbor in neighbors:
-                # todo: 'neighbor in frontier' O(n), needs fixing
-                if neighbor not in grid_separated or grid_separated[neighbor] != plot:
-                    perimeters[plot] += 1
-    print(areas)
-    print(perimeters)
-    return sum(areas[a]*perimeters[p] for a,p in zip(areas.keys(), perimeters.keys()))
-
-
-def p2():
-    unprocessed = copy.deepcopy(grid)
-    plots_with_id = {}
-    id = 0
-    while unprocessed:
-        # print(unprocessed)
-        k,v = unprocessed.popitem()
-        flooded = set()
-        frontier = deque([k])
-        while frontier:
-            point = frontier.popleft()
-            if point in unprocessed:
-                del unprocessed[point]
-            flooded.add(point)
-            neighbors = [vadd(point, dir) for dir in DIRS]
-            for neighbor in neighbors:
-                if neighbor not in grid or neighbor in flooded or neighbor in frontier:
-                    continue
-                if grid[neighbor] == v:
-                    frontier.append(neighbor)
-        plots_with_id[id] = flooded
-        id += 1
-    print(plots_with_id)
-
-    grid_separated = {}
-    for k,v in plots_with_id.items():
-        for point in v:
-            grid_separated[point] = k
-
-    # print(grid_separated)
-
-    areas = {k:len(v) for k,v in plots_with_id.items()}
+    # find all unique fence segments, stored as [plot id] = {(owner tile, owner tile's side), ...}
     perimeters = defaultdict(set)
     for plot, points in plots_with_id.items():
-        print(plot, points)
         for point in points:
             neighbors = [(vadd(point, dir), dir) for dir in DIRS]
             for neighbor, dir in neighbors:
                 if neighbor not in grid_separated or grid_separated[neighbor] != plot:
                     perimeters[plot].add((point, dir))
-    # print(areas)
-    # print(perimeters)
-    # die()
 
+    areas = {k: len(v) for k, v in plots_with_id.items()}
+
+    p1 = sum(areas[a] * len(perimeters[p]) for a, p in zip(areas.keys(), perimeters.keys()))
+
+    # merge all connected fence segments
     merged_fences = {}
     for plot, fence_segments in perimeters.items():
-        # print(plot, fence_segments)
         fences = defaultdict(set)
         fence_id = 0
         while fence_segments:
             frontier = deque([fence_segments.pop()])
             while frontier:
                 point, dir = frontier.popleft()
-                n_dir = (dir[1], dir[0])
+                neighbor_dirs = (dir[1], dir[0])
                 fences[fence_id].add((point, dir))
-                neighbors = [vadd(point, n_dir), vsub(point, n_dir)]
+                neighbors = [vadd(point, neighbor_dirs), vsub(point, neighbor_dirs)]
                 for neighbor in neighbors:
                     neighbor = (neighbor, dir)
-                    # print('neighbor', neighbor, 'fs', fence_segments)
                     if neighbor in fence_segments:
                         fence_segments.remove(neighbor)
                         frontier.append(neighbor)
             fence_id += 1
         merged_fences[plot] = fences
-        # print('fences', plot, fences)
 
-    # print()
-    # print(merged_fences)
-    return sum(areas[a]*len(merged_fences[p]) for a,p in zip(areas.keys(), merged_fences.keys()))
+    p2 = sum(areas[a] * len(merged_fences[p]) for a, p in zip(areas.keys(), merged_fences.keys()))
+    return p1, p2
 
 
 setday(12)
 
 grid, inverse, unique = parsegrid()
 
-verbose = '-v' in sys.argv or '--verbose' in sys.argv
-
-print('part1:', p1() )
-print('part2:', p2() )
+print('part1: %d\npart2: %d' % solve())
