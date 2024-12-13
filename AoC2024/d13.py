e04a7af9a27311f3c92cf6088989382f3d350b0a
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-13 05:55:15 -0500

    Fix 2024 day 13 z3 constraints
    I changed x/y to a/b for clarity.
    
    I accidentally instructed z3 to minimize the wrong variable, which turned out not to matter because the input was crafted for there to only be a single solution for each machine anyway: which is to say, asking for the minimum cost, as it does in the prompt, is nasty misdirection.
    
    However, even after fixing this, specially crafted inputs could still give the wrong answer, which I presume is due to Z3 exploring possibilities with negative button presses. Adding an additional constraint for positive values of a/b fixes this, and this should now produce the correct answer for any possible input, e.g.:
    
    Button A: X+16, Y+16
    Button B: X+32, Y+32
    Prize: X=3360, Y=3360
    
    part1: 105
    part2: 312500000105

--- AoC2024/d13.py
+++ AoC2024/d13.py
@@ -6,18 +6,19 @@ from aoc import *
 def z3_this_nonsense(a_x, a_y, b_x, b_y, prize_x, prize_y, offset=0):
     optimizer = z3.Optimize()
 
-    x, y = z3.Ints('x y')
+    a, b = z3.Ints('a b')
 
-    eq1 = a_x * x + b_x * y == prize_x + offset
-    eq2 = a_y * x + b_y * y == prize_y + offset
+    eq1 = a_x * a + b_x * b == prize_x + offset
+    eq2 = a_y * a + b_y * b == prize_y + offset
 
     optimizer.add(eq1, eq2)
+    optimizer.add(a >= 0, b >= 0)
 
-    optimizer.minimize(y)
+    optimizer.minimize(a)
 
     if optimizer.check() == z3.sat:
         solution = optimizer.model()
-        return solution[x].as_long() * 3 + solution[y].as_long()
+        return solution[a].as_long() * 3 + solution[b].as_long()
     else:
         return 0
 
@@ -27,7 +28,6 @@ def solve(offset):
 
 
 setday(13)
-
 
 with open_default() as file:
     data = [get_ints(line) for line in file.read().split('\n\n')]
