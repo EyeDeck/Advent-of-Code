a1c4bd182181b34f068673cf26da4bd986fd6d21
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-14 02:02:14 -0500

    Wash 2024 day 14
    Part 2 still doesn't produce an answer automatically, but it does par down the possibilities greatly. Evidently the puzzle was generated from a starting position without any overlapping robots, and since only about 1/1615 frames will have zero overlaps (birthday paradox equation, replace 365 with 101*103, and n=500 robots), using that as a target for possible solutions is reasonable. I don't feel like writing something that looks for the tree properly tonight.

--- AoC2024/d14.py
+++ AoC2024/d14.py
@@ -3,21 +3,19 @@ import math
 from aoc import *
 
 
+def tick(robots, n):
+    new_robots = []
+    for robot in robots:
+        nxt = vadd(robot[0], vmul(robot[1], (n, n)))
+        nxt = (nxt[0] % WIDTH, nxt[1] % HEIGHT)
+        new_robots.append([nxt, robot[1]])
+    return new_robots
+
+
 def p1():
-    def tick(robots, n):
-        new_robots = []
-        for robot in robots:
-            nxt = vadd(robot[0], vmul(robot[1],(n,n)))
-            nxt = (nxt[0] % WIDTH, nxt[1] % HEIGHT)
-            new_robots.append([nxt, robot[1]])
-        return new_robots
+    r = tick(robots, 100)
 
-    robots = []
-    for i, line in enumerate(data):
-        robots.append([(line[0],line[1]), (line[2], line[3])])
-
-    robots = tick(robots, 100)
-    positions = [k[0] for k in robots]
+    positions = [k[0] for k in r]
     quadrants = [
         [p for p in positions if p[0] < WIDTH//2 and p[1] < HEIGHT//2],
         [p for p in positions if p[0] < WIDTH//2 and p[1] > HEIGHT//2],
@@ -25,44 +23,38 @@ def p1():
         [p for p in positions if p[0] > WIDTH//2 and p[1] > HEIGHT//2]
     ]
 
-    # print_2d('.', {k:'#' for k in positions})
-    print_2d('.', {k:'A' for k in quadrants[0]}, {k:'B' for k in quadrants[1]}, {k:'C' for k in quadrants[2]}, {k:'D' for k in quadrants[3]})
+    if verbose:
+        print_2d('.', {k:'A' for k in quadrants[0]}, {k:'B' for k in quadrants[1]}, {k:'C' for k in quadrants[2]}, {k:'D' for k in quadrants[3]})
 
-    print(robots)
-    print(quadrants)
     return math.prod(len(q) for q in quadrants)
 
 
 def p2():
-    def tick(robots, n):
-        new_robots = []
-        for robot in robots:
-            nxt = vadd(robot[0], vmul(robot[1], (n, n)))
-            nxt = (nxt[0] % WIDTH, nxt[1] % HEIGHT)
-            new_robots.append([nxt, robot[1]])
-        return new_robots
-
     robots = []
     for i, line in enumerate(data):
         robots.append([(line[0], line[1]), (line[2], line[3])])
 
-    for i in range(10000):
+    i = 0
+    while True:
+        i += 1
         robots = tick(robots, 1)
-        print(i+1)
+        print(i, end='\r')
         positions = [k[0] for k in robots]
-        print_2d('.', {k:'#' for k in positions})
-        input()
+        if len(positions) == len(set(positions)):
+            print_2d('.', {k:'#' for k in positions})
+            if input(f'{i} | enter (y) if this is a tree: ').lower().strip() == 'y':
+                return i
 
 
 setday(14)
 
 data = parselines(get_ints)
+robots = []
+for i, line in enumerate(data):
+    robots.append([(line[0], line[1]), (line[2], line[3])])
 
 WIDTH = 101
 HEIGHT = 103
-
-# WIDTH = 11
-# HEIGHT = 7
 
 verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
