1f2067e077eb197146f7667de8b0b02b3c963298
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-21 08:11:37 -0500

    Solve 2024 day 21
    05:22:03 (4145) / 08:09:15 (3308)
    Oh my god that was tedious

--- AoC2024/d21.py
+++ AoC2024/d21.py
@@ -5,19 +5,6 @@ import networkx as nx
 
 
 def p1():
-    def get_all_dists(d):
-        rd = defaultdict(dict)
-        for k_a, v_a in d.items():
-            for k_b, v_b in d.items():
-                # print(k_a, v_a, k_b, v_b)
-                rd[v_a][v_b] = vdistm(k_a, k_b)
-        return rd
-
-    num_pad_dists = get_all_dists(num_pad)
-    dir_pad_dists = get_all_dists(dir_pad)
-    print(num_pad_dists)
-    print(dir_pad_dists)
-
     # NUMPAD <- DIRPAD <- DIRPAD <- DIRPAD
 
     def get_neighbors(node, g):
@@ -34,29 +21,13 @@ def p1():
         for k, v in d.items():
             for n, c in get_neighbors(k, d):
                 G.add_edge(v, d[n], weight=1, dir=c)
-                print(G, k, v, n, d[n])
+                # print(G, k, v, n, d[n])
         return G
 
     num_pad_graph = make_graph(num_pad)
     dir_pad_graph = make_graph(dir_pad)
-    print(num_pad_graph)
-    print(dir_pad_graph)
-
-    # for line in data:
-    #     print(line)
-    #     line = 'A' + line
-    #     numpad_steps = [(line[i], line[i+1]) for i in range(len(line)-1)]
-    #     print(numpad_steps)
-    #     for numpad_step in numpad_steps:
-    #         print(numpad_step)
-    #         numpad_step_paths = [p for p in nx.all_shortest_paths(num_pad_graph, *numpad_step)]
-    #         print(numpad_step_paths)
-    #         for numpad_path in numpad_step_paths:
-    #             numpad_path_steps = [(numpad_path[i], numpad_path[i + 1]) for i in range(len(numpad_path) - 1)]
-    #             print(numpad_path_steps)
-    #             for numpad_path_step in numpad_path_steps:
-    #                 print(numpad_path_step)
-    #                 die()
+    # print(num_pad_graph)
+    # print(dir_pad_graph)
 
     acc = 0
     for line in data:
@@ -73,6 +44,7 @@ def p1():
             print('shortest paths from', numpad_step)
             for digit_path in nx.all_shortest_paths(num_pad_graph, *numpad_step):
                 path = [num_pad_graph[digit_path[i]][digit_path[i + 1]]["dir"] for i in range(len(digit_path) - 1)]
+                print(path)
                 path.append('A')
                 path.insert(0, 'A')
 
@@ -94,10 +66,15 @@ def p1():
         print(optimized_possibilities)
 
         shortest = INF
+        best_path = ''
         for possibility in itertools.product(*optimized_possibilities):
             possibility = 'A' + ''.join(possibility)
             three = ''.join([keypad_map[possibility[i]][possibility[i + 1]] for i in range(len(possibility) - 1)])
+            if len(three) < shortest:
+                shortest = len(three)
+                best_path = three
             shortest = min(shortest, len(three))
+        print(best_path)
 
         # print(f'after 2 robots for {line}:')
         # best = ''.join(best)
@@ -118,28 +95,6 @@ def p1():
 
     return acc
 
-'''
-after 3 robots for A980A:
-v<<A>>^AAAvA^Av<A<AA>>^AvAA^<A>Av<A<A>>^AAA<A>vA^Av<A^>A<A>A
-980A:
-<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
-
-179A:
-<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-v<<A>>^Av<A<A>>^AAvAA^<A>Av<<A>>^AAvA^Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-
-<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
-v<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>A<A>Av<A^>A<A>Av<A<A>>^AA<A>vA^A
-
-v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-
-<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
-'''
-
-# keypad_map = {'^': {'A': '>A', '>': 'v'}
-#               }
 
 # keypad_map = {
 #     '^': {'^': 'A', 'A': '>A', 'v': 'vA', '<': 'v<A', '>': ['v>A', '>vA']},
@@ -151,41 +106,163 @@ v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<A>vA^A
 
 keypad_map = {
     '^': {'^': 'A', 'A': '>A',   'v': 'vA',  '<': 'v<A',  '>': 'v>A'},
-    'A': {'A': 'A', '^': '<A',   'v': '<vA', '<': 'v<<A', '>': 'vA' },
+    'A': {'A': 'A', '^': '<A',   'v': '<vA', '<': 'v<<A', '>': 'vA'},
     '<': {'<': 'A', 'A': '>>^A', '^': '>^A', 'v': '>A',   '>': '>>A'},
-    'v': {'v': 'A', 'A': '>^A',  '^': '^A',  '<': '<A',   '>':  '>A'},
+    'v': {'v': 'A', 'A': '>^A',  '^': '^A',  '<': '<A',   '>': '>A'},
     '>': {'>': 'A', 'A': '^A',   '^': '<^A', 'v': '<A',   '<': '<<A'}
 }
 
-
-# def get_numpad_steps(a, b):
-#     steps = []
-#     a_x, a,y = num_pad_inv[a]
-#     b_x, b_y = num_pad_inv[b]
-#     if a_x < b_x:
-#
-#     elif a_x > b_x:
-#         pass
-#     return steps
-
-
-# def p1():
-#     for line in data:
-#         print(line)
-#         line = 'A' + line
-#         numpad_steps = [(line[i], line[i+1]) for i in range(len(line)-1)]
-#         print(numpad_steps)
-#     return None
+keypad_map2 = {
+    '^': {'^': 'A', 'A': '>A', 'v': 'vA', '<': 'v<A', '>': ['v>A', '>vA']},
+    'A': {'A': 'A', '^': '<A', 'v': ['v<A', '<vA'], '<': 'v<<A', '>': 'vA'},
+    '<': {'<': 'A', 'A': '>>^A', '^': '>^A', 'v': '>A', '>': '>>A'},
+    'v': {'v': 'A', 'A': ['^>A', '>^A'], '^': '^A', '<': '<A', '>': '>A'},
+    '>': {'>': 'A', 'A': '^A', '^': ['<^A', '^<A'], 'v': '<A', '<': '<<A'}
+}
 
 
 def p2():
-    return None
+
+
+    # NUMPAD <- DIRPAD <- DIRPAD <- DIRPAD
+
+    def get_neighbors(node, g):
+        neighbors = []
+        for dir in DIRS:
+            n = vadd(node, dir)
+            if n in g:
+                # print(DIR_MAP)
+                neighbors.append((n, DIR_MAP[dir]))
+        return neighbors
+
+    def make_graph(d):
+        G = nx.DiGraph()
+        for k, v in d.items():
+            for n, c in get_neighbors(k, d):
+                G.add_edge(v, d[n], weight=1, dir=c)
+                # print(G, k, v, n, d[n])
+        return G
+
+    num_pad_graph = make_graph(num_pad)
+
+    @memo
+    def min_steps(a, b, n):
+        poss = keypad_map[a][b]
+        if n == 0:
+            return len(poss)
+
+        poss = 'A' + poss
+        # print('POSS', poss)
+        total = 0
+        for i in range(len(poss) - 1):
+            total += min_steps(poss[i], poss[i + 1], n - 1)
+        # print('returning', total, 'for', a, 'to', b)
+        return total
+
+    @memo
+    def min_steps(fro, to, n):
+        posses = keypad_map2[fro][to]
+        if not isinstance(posses, list):
+            posses = [posses]
+        if n == 0:
+            return min(len(s) for s in posses)
+
+        totals = []
+        for piss in posses:
+            piss = 'A' + piss
+            # print('PISS', piss)
+            total = 0
+            for i in range(len(piss) - 1):
+                total += min_steps(piss[i], piss[i + 1], n - 1)
+            # print('returning', total, 'for', fro, to, n)
+            totals.append(total)
+        return min(totals)
+
+
+    acc = 0
+    for line in data:
+        print(line)
+        line = 'A' + line
+        numpad_steps = [(line[i], line[i + 1]) for i in range(len(line) - 1)]
+        # print(numpad_steps)
+        expanded_steps = []
+        best = []
+        step_total = 0
+        for numpad_step in numpad_steps:
+            # print()
+            # expanded_steps.append()
+            paths = []
+            # print('shortest paths from', numpad_step)
+            for digit_path in nx.all_shortest_paths(num_pad_graph, *numpad_step):
+                # path = [num_pad_graph[digit_path[i]][digit_path[i + 1]]["dir"] for i in range(len(digit_path) - 1)]
+                path = 'A' + ''.join(num_pad_graph[digit_path[i]][digit_path[i + 1]]["dir"] for i in range(len(digit_path) - 1))
+                # print(path)
+                # path.append('A')
+                # path.insert(0, 'A')
+
+                # print(digit_path, path)
+                # paths.append(''.join([keypad_map[path[i]][path[i+1]] for i in range(len(path) - 1)]))
+                # print([[path[i], path[i+1]] for i in range(len(path)-1)])
+                paths.append(path)
+            # print(paths)
+            expanded_steps.append(paths)
+            # best.append(min(paths, key=len))
+        # print(f'possibilities for {line}:')
+        # print('ex_steps', expanded_steps)
+        # expanded_steps = [min(n) for n in expanded_steps]
+        # print(expanded_steps)
+        # print()
+        # step_total = sum(expanded_steps)
+
+        shortest = INF
+        for possibility in itertools.product(*expanded_steps):
+            possibility = ''.join(possibility) + 'A'
+            # print('possibility!', possibility)
+            sums = [min_steps(possibility[i], possibility[i + 1], 24) for i in range(len(possibility) - 1)]
+            # print(sums)
+            this_n = sum(sums)
+            shortest = min(shortest, this_n)
+        #     three = ''.join([keypad_map[possibility[i]][possibility[i + 1]] for i in range(len(possibility) - 1)])
+        #     if len(three) < shortest:
+        #         shortest = len(three)
+        #         best_path = three
+        #     shortest = min(shortest, len(three))
+        # print(best_path)
+
+        # sum([min_steps(path[i], path[i + 1], 2) for i in range(len(path) - 1)])
+
+        # optimized_possibilities = []
+        # for list_of_possiblities in expanded_steps:
+        #     # print(list_of_possiblities)
+        #     mn = len(min(list_of_possiblities, key=len))
+        #     optimized_possibilities.append([l for l in list_of_possiblities if len(l) == mn])
+        #     # print(mn)
+        #
+        # print(f'possibilities for {line[1:]}:')
+        # print(optimized_possibilities)
+        #
+        #
+        #
+        # print(min_steps('^', '>', 25))
+        #
+        # shortest = INF
+        # for possibility in itertools.product(*optimized_possibilities):
+        #     possibility = 'A' + ''.join(possibility)
+        #     three = ''.join([keypad_map[possibility[i]][possibility[i + 1]] for i in range(len(possibility) - 1)])
+        #     print(three)
+        #     shortest = min(shortest, len(three))
+
+        num_part = int(''.join(re.findall(r'\d', line)))
+        acc += num_part * shortest  #shortest
+
+
+    return acc
 
 
 setday(21)
 
 data = parselines()
-
+# data = [data[0]]
 
 def make_pad(s):
     grid = {}
@@ -202,11 +279,11 @@ DIR_MAP = {v: k for k, v in {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0
 
 num_pad, num_pad_inv = make_pad('789,456,123, 0A'.split(','))
 dir_pad, dir_pad_inv = make_pad(' ^A,<v>'.split(','))
-print(num_pad, dir_pad)
+# print(num_pad, dir_pad)
 
 verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
-print('part1:', p1())
+# print('part1:', p1())
 print('part2:', p2())
 
 # print('part1: %d\npart2: %d' % solve())
