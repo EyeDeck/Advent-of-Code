1c25f5feee365e4aa4853e70a276bb7a1b92ce04
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-23 01:09:10 -0500

    Wash 2024 day 23

--- AoC2024/d23.py
+++ AoC2024/d23.py
@@ -1,6 +1,4 @@
-import itertools
 import networkx as nx
-
 from aoc import *
 
 
@@ -10,28 +8,16 @@ def solve():
         a, b = line.split('-')
         G.add_edge(a, b)
 
-    sets = set()
-    for node in G.nodes():
-        if node[0] != 't':
-            continue
-        neighbors = [n for n in G.neighbors(node)]
-        sets |= {frozenset([node, a, b]) for a, b in itertools.combinations(neighbors, 2)}
+    cliques = [c for c in nx.enumerate_all_cliques(G)]
 
-    interconnected = set()
-    for s in sets:
-        a, b, c = s
-        if G.has_edge(a, b) and G.has_edge(b, c) and G.has_edge(a, c):
-            interconnected.add(s)
-
-    yield len(interconnected)
-
-    yield ','.join(sorted(sorted(list(nx.find_cliques(G)), key=len, reverse=True)[0]))
+    return \
+        len(list(c for c in cliques if len(c) == 3 and any(node[0] == 't' for node in c))), \
+        ','.join(sorted(max(cliques, key=len)))
 
 
 setday(23)
 
 data = parselines()
 
-gen = solve()
-print('part1:', next(gen))
-print('part2:', next(gen))
+print('part1: %d\npart2: %s' % solve())
+
