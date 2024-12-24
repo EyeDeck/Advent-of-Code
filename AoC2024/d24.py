66e114eebab69026b1cdf91f81d6e4897ec6589e
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2024-12-24 07:32:29 -0500

    Wash 2024 day 24
    Fixed some bugs and improved my render. Still much room for improvement.

--- AoC2024/d24.py
+++ AoC2024/d24.py
@@ -4,14 +4,10 @@ import operator
 import networkx as nx
 from pyvis.network import Network
 
-import matplotlib.pyplot as plt
-
 from aoc import *
 
 
-def p1():
-    logic = copy.deepcopy(logic_data)
-    wires = copy.deepcopy(wire_data)
+def run_sim(logic, wires):
     while logic:
         to_pop = []
         for i, entry in logic.items():
@@ -20,14 +16,12 @@ def p1():
                 logic[i][1] = wires[a]
             if b in wires:
                 logic[i][2] = wires[b]
-            if isinstance(a, bool) and isinstance(b, bool):
-                wires[out] = op(a, b)
+            if isinstance(a, int) and isinstance(b, int):
+                wires[out] = logic_map[op](a, b)
                 to_pop.append(i)
-                continue
         for i in to_pop:
             del logic[i]
-
-    return wires_to_int(get_wires_starting_with(wires, 'z'))
+    return wires
 
 
 def get_wires_starting_with(wires, c):
@@ -35,44 +29,11 @@ def get_wires_starting_with(wires, c):
 
 
 def wires_to_int(wires):
-    return sum((1 << i + 1 if wires[k] else 0) for i, k in enumerate(sorted(wires.keys(), reverse=True)))
+    return sum(v << get_ints(k)[0] for k, v in wires.items())
 
 
-# 0000000000000000010100101110001101110001011111001010000010101000
-# 0000000000000000001100111011000111000100000011101111101010100110
-def p2():
-    logic = copy.deepcopy(logic_data)
-    wires = copy.deepcopy(wire_data)
-
-    x_wires = get_wires_starting_with(wires, 'x')
-    y_wires = get_wires_starting_with(wires, 'y')
-
-    input_x = wires_to_int(x_wires)
-    input_y = wires_to_int(y_wires)
-    print(input_x, input_y, input_x + input_y)
-
-    print('x', x_wires)
-    print('y', y_wires)
-    while logic:
-        to_pop = []
-        for i, entry in logic.items():
-            op, a, b, out = entry
-            if a in wires:
-                logic[i][1] = wires[a]
-            if b in wires:
-                logic[i][2] = wires[b]
-            if isinstance(a, bool) and isinstance(b, bool):
-                wires[out] = op(a, b)
-                to_pop.append(i)
-                continue
-        for i in to_pop:
-            del logic[i]
-
-    real_in = input_x + input_y
-    real_out = wires_to_int(get_wires_starting_with(wires, 'z'))
-    print(format(real_in, '064b'))
-    print(format(real_out, '064b'))
-    # print(bin(real_out))
+def p1():
+    return wires_to_int(get_wires_starting_with(run_sim(copy.deepcopy(logic_data), copy.deepcopy(wire_data)), 'z'))
 
 
 def p2():
@@ -85,51 +46,37 @@ def p2():
         ['wpq', 'grf'],
     ]
 
-    revmap = {k: v for k, v in swaps} | {v: k for k, v in swaps}
-    print(revmap)
+    swap_map = {k: v for k, v in swaps} | {v: k for k, v in swaps}
 
-    logic = {
-        i: [
-            logic_map[g[1]],
-            g[0],
-            g[2],
-            (revmap[g[4]] if g[4] in revmap else g[4])
-        ] for i, g in enumerate([line.split() for line in logic_raw.split('\n')])}
-
-    # print(logic==logic_data)
+    logic = copy.deepcopy(logic_data)
+    for i, gate in logic.items():
+        if gate[3] in swap_map:
+            gate[3] = swap_map[gate[3]]
 
     x_wires = get_wires_starting_with(wires, 'x')
     y_wires = get_wires_starting_with(wires, 'y')
 
     input_x = wires_to_int(x_wires)
     input_y = wires_to_int(y_wires)
-    print(input_x, input_y, input_x + input_y)
 
     G = nx.DiGraph()
     gate_cts = {'XOR': 0, 'AND': 0, 'OR': 0}
     for i, gate in logic.items():
-        print(gate)
         op, a, b, out = gate
-        op = logic_map_i[op]
         gate_node = op + str(gate_cts[op])
         gate_cts[op] += 1
-        G.add_edge(a, gate_node)
-        G.add_edge(b, gate_node)
-        G.add_edge(gate_node, out)
+        G.add_edge(a, gate_node, weight=(1.5 if a[0]=='x' else (1.7 if op=='XOR' else 1)))
+        G.add_edge(b, gate_node, weight=(1.5 if b[0]=='x' else (1.7 if op=='XOR' else 1)))
+        G.add_edge(gate_node, out, weight=(1.5 if out[0]=='z' else 1))
 
-    print(G)
+    net = Network(notebook=False, directed=True, bgcolor='#222222', font_color='white', width='100%', height='100vh',
+                  cdn_resources='remote')
 
-    # nx.draw_kamada_kawai(G, with_labels=True)
-    # # nx.draw_planar(G, with_labels=True)
-    # # nx.draw_spectral(G, with_labels=True)  # useless
-    # # nx.draw_spring(G, with_labels=True)  # useless
-    # # nx.draw_shell(G, with_labels=True)
-    # plt.show()
-
-    net = Network(notebook=False, directed=True, bgcolor='#222222', font_color='white', width='100%', height='100vh', cdn_resources='remote')
-
+    print('Calculating graph...', end='\r', flush=True)
     pos = nx.kamada_kawai_layout(G, scale=5000)
-    # pos = nx.planar_layout(G, scale=100)
+    # pos = nx.forceatlas2_layout(G)
+    # pos = nx.spiral_layout(G, equidistant=True, resolution=30000)
+    # pos = nx.arf_layout(G, scaling=1000.0)
 
     node_colors = {
         'x': '#00FF90',
@@ -142,13 +89,11 @@ def p2():
     }
 
     for node, position in pos.items():
-        print(node)
-        print(G[node])
         x, y = position
         net.add_node(
             node,
             label=(re.sub(r'\d', '', node) if node[0] in 'XAO' else node),
-            x=x, y=y,
+            x=int(x), y=int(y),
             physics=False,
             shape='ellipse',
             color=(node_colors[node[0]] if node[0] in node_colors else node_colors[None]),
@@ -159,53 +104,23 @@ def p2():
 
     net.write_html('d24_graph_visualization.html')
 
-    while logic:
-        to_pop = []
-        for i, entry in logic.items():
-            op, a, b, out = entry
-            if a in wires:
-                logic[i][1] = wires[a]
-            if b in wires:
-                logic[i][2] = wires[b]
-            if isinstance(a, bool) and isinstance(b, bool):
-                wires[out] = op(a, b)
-                to_pop.append(i)
-                continue
-        for i in to_pop:
-            del logic[i]
-
     real_in = input_x + input_y
-    real_out = wires_to_int(get_wires_starting_with(wires, 'z'))
-    print(format(real_in, '064b'))
-    print(format(real_out, '064b'))
-    # print(bin(real_out))
+    real_out = wires_to_int(get_wires_starting_with(run_sim(logic, wires), 'z'))
+
+    assert real_in == real_out, 'gates are still buggy'
 
     return ','.join(sorted([x for xs in swaps for x in xs]))
 
 
 setday(24)
 
-# data = parselines()
-# data = parselines(get_ints)
-# grid, inverse, unique = parsegrid()
-
 with open_default() as file:
     wires_raw, logic_raw = file.read().split('\n\n')
-# wires = {k:v=='1' for k,v in [wire for wire in ]}
-wire_data = {k: v == '1' for k, v in [line.split(': ') for line in wires_raw.split('\n')]}
-# print([line.split() for line in logic_raw.split('\n')])
+
+wire_data = {k: int(v) for k, v in [line.split(': ') for line in wires_raw.split('\n')]}
+logic_data = {i: [g[1], g[0], g[2], g[4]] for i, g in enumerate([line.split() for line in logic_raw.split('\n')])}
 
 logic_map = {'XOR': operator.xor, 'AND': operator.and_, 'OR': operator.or_}
-logic_map_i = {v: k for k, v in logic_map.items()}
-logic_data = {i: [logic_map[g[1]], g[0], g[2], g[4]] for i, g in
-              enumerate([line.split() for line in logic_raw.split('\n')])}
-
-# print(wires)
-# print(logic)
-
-verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
 print('part1:', p1())
 print('part2:', p2())
-
-# print('part1: %d\npart2: %d' % solve())
