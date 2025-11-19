991cf86ca242d23b3654b27a4a1fd7f8ee41a1be
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-11-19 18:53:53 -0500

    Wash Everybody Codes 2025 quest 13
    DRYed the code out, and fixed the wasteful search for '1' that required looping over the array again (which I only left in because I didn't want to have to deal with debugging off-by-ones while going fast).
    
    There's a smarter way to do this without having to build the entire array with bruteforce, but I probably can't be bothered.

--- EC2025/q13.py
+++ EC2025/q13.py
@@ -3,64 +3,30 @@ from collections import deque
 from ec import *
 
 
-def p1():
-    data = [line[0] for line in parse_lines(1, get_ints)]
-    # print(data)
+def bruteforce(n, turns, p1=False):
+    if p1:
+        data = [(line[0], line[0]) for line in parse_lines(n, get_ints)]
+    else:
+        data = [tuple(int(s) for s in line.split('-')) for line in parse_lines(n)]
+
     wheel = deque([1])
     toggle = True
-    for n in data:
+    start_pos = 0
+
+    for l, r in data:
         if toggle:
-            wheel.append(n)
+            wheel.extend(i for i in range(l, r + 1))
         else:
-            wheel.appendleft(n)
+            wheel.extendleft(i for i in range(l, r + 1))
+            start_pos += r - l + 1
         toggle = not toggle
-    start_pos = wheel.index(1)
-    next_pos = (start_pos + 2025) % len(wheel)
-    # print(wheel, start_pos, wheel[next_pos])
-    # data =
-    # data = parse_double_break(1)
 
-    return wheel[next_pos]
-
-
-def p2():
-    data = [tuple(int(s) for s in line.split('-')) for line in parse_lines(2)]
-    # print(data)
-    wheel = deque([1])
-    toggle = True
-    for n in data:
-        if toggle:
-            wheel.extend([i for i in range(n[0], n[1]+1)])
-        else:
-            wheel.extendleft([i for i in range(n[0], n[1]+1)])
-        toggle = not toggle
-    start_pos = wheel.index(1)
-    next_pos = (start_pos + 20252025) % len(wheel)
-    # print(wheel, start_pos)
-
-    return wheel[next_pos]
-
-
-def p3():
-    data = [tuple(int(s) for s in line.split('-')) for line in parse_lines(3)]
-    # print(data)
-    wheel = deque([1])
-    toggle = True
-    for n in data:
-        if toggle:
-            wheel.extend([i for i in range(n[0], n[1]+1)])
-        else:
-            wheel.extendleft([i for i in range(n[0], n[1]+1)])
-        toggle = not toggle
-    start_pos = wheel.index(1)
-    next_pos = (start_pos + 202520252025 ) % len(wheel)
-    # print(wheel, start_pos)
-
+    next_pos = (start_pos + turns) % len(wheel)
     return wheel[next_pos]
 
 
 setquest(13)
 
-print('part1:', p1())
-print('part2:', p2())
-print('part3:', p3())
+print('part1:', bruteforce(1, 2025, True))
+print('part2:', bruteforce(2, 20252025))
+print('part3:', bruteforce(3, 202520252025))
