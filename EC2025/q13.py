222817aaf8fdee59704ef8fe458c2c13153ff6fe
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-11-19 23:42:28 -0500

    Optimize Everybody Codes 2025 quest 13
    A way to compute the answer in O(n) time that was only a slight modification to my old code occurred to me later while I was idly contemplating the problem. This is literally something like 100,000x faster in part 3.

--- EC2025/q13.py
+++ EC2025/q13.py
@@ -3,30 +3,43 @@ from collections import deque
 from ec import *
 
 
-def bruteforce(n, turns, p1=False):
+def fast(n, turns, p1=False):
     if p1:
         data = [(line[0], line[0]) for line in parse_lines(n, get_ints)]
     else:
         data = [tuple(int(s) for s in line.split('-')) for line in parse_lines(n)]
 
-    wheel = deque([1])
+    ranges_sorted = deque([(1, (1, 1))])
     toggle = True
+    total_len = 1
     start_pos = 0
 
     for l, r in data:
+        ln = r - l + 1
+        total_len += ln
         if toggle:
-            wheel.extend(i for i in range(l, r + 1))
+            ranges_sorted.append((ln, (l, r)))
         else:
-            wheel.extendleft(i for i in range(l, r + 1))
-            start_pos += r - l + 1
-        toggle = not toggle
+            ranges_sorted.appendleft((ln, (r, l)))
+            start_pos += ln
 
-    next_pos = (start_pos + turns) % len(wheel)
-    return wheel[next_pos]
+        toggle = not toggle
+    end_pos = (start_pos + turns) % total_len
+
+    acc = 0
+    for ln, (l, r) in ranges_sorted:
+        if ln + acc < (end_pos + 1):
+            acc += ln
+        else:
+            range_index = end_pos - acc
+            if l < r:
+                return l + range_index
+            else:
+                return l - range_index
 
 
 setquest(13)
 
-print('part1:', bruteforce(1, 2025, True))
-print('part2:', bruteforce(2, 20252025))
-print('part3:', bruteforce(3, 202520252025))
+print('part1:', fast(1, 2025, True))
+print('part2:', fast(2, 20252025))
+print('part3:', fast(3, 202520252025))
