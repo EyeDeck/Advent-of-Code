3c1945751a372857f4635346bc06262baad847f4
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-11-29 01:56:08 -0500

    Wash Everybody Codes 2025 quest 20
    Part 1 actually works properly (and doesn't just coincidentally usually work) now, too.

--- EC2025/q20.py
+++ EC2025/q20.py
@@ -1,18 +1,42 @@
 from ec import *
 
 
+def gen_neighbors(grid, width=0):
+    all_neighbors = {}
+    for pos in grid:
+        neighbors = []
+
+        if width:
+            r_pos = rotate(pos, width)
+        else:
+            r_pos = pos
+
+        if (r_pos[0] + r_pos[1]) & 1:
+            dirs = [(-1, 0), (1, 0), (0, 1)]
+        else:
+            dirs = [(-1, 0), (1, 0), (0, -1)]
+
+        if width:
+            dirs.append((0, 0))
+
+        for offset in dirs:
+            n_pos = vadd(r_pos, offset)
+            if n_pos in grid and grid[n_pos] in {'T', 'S', 'E'}:
+                neighbors.append(n_pos)
+
+        all_neighbors[pos] = neighbors
+    return all_neighbors
+
+
 def p1():
     grid, inverse, unique = parse_grid(1)
+    grid = {k:v for k,v in grid.items() if v=='T'}
 
-    dirs = [(-1, 0), (1, 0), (0, -1)]
+    neighbors = gen_neighbors(grid)
+
     acc = 0
-    for pos in grid:
-        if grid[pos] != 'T':
-            continue
-        for o in dirs:
-            other_pos = vadd(pos, o)
-            if other_pos in grid and grid[other_pos] == 'T':
-                acc += 1
+    for n in neighbors.values():
+        acc += len(n)
 
     return acc // 2
 
@@ -20,83 +44,36 @@ def p1():
 def p2():
     grid, inverse, unique = parse_grid(2)
 
-    def get_neighbors(pos):
-        neighbors = []
-        if (pos[0] + pos[1]) & 1:
-            dirs = [(-1, 0), (1, 0), (0, 1)]
-        else:
-            dirs = [(-1, 0), (1, 0), (0, -1)]
-        for o in dirs:
-            other_pos = vadd(pos, o)
-            if other_pos in grid and grid[other_pos] in {'T', 'S', 'E'}:
-                print(grid[other_pos])
-                neighbors.append((other_pos, 1))
-        print(pos, neighbors)
-        return neighbors
+    neighbors = gen_neighbors(grid)
 
-    print(unique['S'], unique['E'])
-    path = wbfs(unique['S'], unique['E'], get_neighbors)
-    print(path)
+    path = bfs(unique['S'], unique['E'], neighbors)
 
-    print_2d('.', grid, {k: '@' for k in path})
+    if verbose:
+        print_2d('.', grid, {k: '@' for k in path})
 
     return len(path) - 1
+
+
+def rotate(pos, width):
+    # jesus christ
+    x, y = pos
+    y_r = (width - x - y) // 2
+    x_r = y_r + y * 2 + (x - y) % 2
+    return x_r, y_r
 
 
 def p3():
     grid, inverse, unique = parse_grid(3)
 
-    def get_neighbors(pos):
-        print('started at', pos)
-        pos = rotate(pos)
-        print('rotated to', pos)
-        neighbors = []
-        if (pos[0] + pos[1]) & 1:
-            print('which is an up triangle')
-            dirs = [(0, 0), (-1, 0), (1, 0), (0, 1)]
-        else:
-            print('which is a down triangle')
-            dirs = [(0, 0), (-1, 0), (1, 0), (0, -1)]
-        for o in dirs:
-            other_pos = vadd(pos, o)
-            print('checking coord', other_pos)
-            if other_pos in grid and grid[other_pos] in {'T', 'S', 'E'}:
-                print(grid[other_pos])
-                neighbors.append((other_pos, 1))
-        print('and has neighbors', neighbors)
-        return neighbors
-
     bounds = grid_bounds(grid)
     width, height = bounds[2:]
 
-    def rotate(pos):
-        # jesus christ
-        x, y = pos
-        y_r = (width - x - y) // 2
-        x_r = y_r + y * 2 + (x - y) % 2
-        return x_r, y_r
+    neighbors = gen_neighbors(grid, width)
 
-    rotated_grid = {}
-    for pos, v in grid.items():
-        if v == '.':
-            continue
-        rotated_grid[rotate(pos)] = v
-    # print_2d('.', grid, {pos:'A', r_pos:'B'})
-    print_2d('.', rotated_grid)
+    path = bfs(unique['S'], unique['E'], neighbors)
 
-    rotated_grid2 = {}
-    for pos, v in rotated_grid.items():
-        if v == '.':
-            continue
-        rotated_grid2[rotate(pos)] = v
-    # print_2d('.', grid, {pos:'A', r_pos:'B'})
-    print_2d('.', rotated_grid2)
-
-    print(unique['S'], unique['E'])
-    path = wbfs(unique['S'], unique['E'], get_neighbors)
-    print(path)
-
-    print_2d('.', grid, {k: '@' for k in path})
+    if verbose:
+        print_2d('.', grid, {k: '@' for k in path})
 
     return len(path) - 1
 
