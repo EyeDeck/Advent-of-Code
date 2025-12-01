6d2653a16d70f4780d5af3c1c54ba580b51a5b7c
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-11-30 23:26:13 -0500

    Commit some half-finished changes from ages ago
    I don't remember what I was doing with these anymore. Maybe I'll get back to them.

--- AoC2020/d20.py
+++ AoC2020/d20.py
@@ -1,284 +1,179 @@
-import math
+import collections
 import random
-import re
-import sys
-from collections import defaultdict
-
-import numpy as np
-
-from aoc import memo
+from math import *
+from aoc import *
 
 
-def get_int(bit_array, rev):
-    if rev:
-        return sum(v << i for i, v in enumerate(bit_array))
+def grid_slice(d, start, end):
+    (x1, y1), (x2, y2) = start, end
+    if x1 == x2 and y1 != y2:
+        step = (0, 1 if y2 > y1 else -1)
+    elif x1 != x2 and y1 == y2:
+        step = (1 if x2 > x1 else -1, 0)
     else:
-        return sum(v << i for i, v in enumerate(reversed(bit_array)))
+        raise ValueError('Slice should be a straight line')
+
+    p = start
+    l = [d[p]]
+    # print(step)
+    while True:
+        p = vadd(p, step)
+        l.append(d[p])
+        if p == end:
+            break
+    return l
 
 
-def get_edge(tile, direction, rev=False):
-    if direction == 0:  # UP
-        if rev:
-            return get_int(tile[0, :], True)
-        else:
-            return get_int(tile[0, :], False)
-    if direction == 2:  # DOWN
-        if rev:
-            return get_int(tile[-1, :], True)
-        else:
-            return get_int(tile[-1, :], False)
-    if direction == 3:  # LEFT
-        if rev:
-            return get_int(tile[:, 0], True)
-        else:
-            return get_int(tile[:, 0], False)
-    if direction == 1:  # RIGHT
-        if rev:
-            return get_int(tile[:, -1], True)
-        else:
-            return get_int(tile[:, -1], False)
+def solve():
+    edges_to_ids = defaultdict(set)
+    ids_to_edges = defaultdict(set)
+
+    # hard_coded, oh well
+    edge_steps = [
+        [(0, 0), (9, 0)],
+        [(9, 0), (9, 9)],
+        [(0, 9), (9, 9)],
+        [(0, 0), (0, 9)]
+    ]
+
+    for piece_id, piece_grid in data.items():
+        # print_2d(' ', piece_grid)
+        x1, y1, x2, y2 = grid_bounds(piece_grid)
+
+        edges = [grid_slice(piece_grid, start_pos, end_pos) for start_pos, end_pos in edge_steps]
+        for edge in edges:
+            s, s_r = ''.join(edge), ''.join(reversed(edge))
+            edges_to_ids[s].add(piece_id)
+            edges_to_ids[s_r].add(piece_id)
+            ids_to_edges[piece_id].add(s)
+            ids_to_edges[piece_id].add(s_r)
+    edges_with_connections = {k: v for k, v in edges_to_ids.items() if len(v) == 2}
+    edges_without_connections = collections.Counter([list(v)[0] for k, v in edges_to_ids.items() if len(v) == 1])
+    corner_tiles = [k for k, v in edges_without_connections.items() if v == 4]
+    edge_tiles = [k for k, v in edges_without_connections.items() if v == 2]
+
+    # print(edges_to_ids)
 
 
-def apply_orientation(arr, rotation, flip_v, flip_h):
-    arr_copy = np.copy(arr)
-    if rotation:
-        arr_copy = np.rot90(arr_copy, rotation)
-    if flip_v:
-        arr_copy = np.flip(arr_copy, 1)
-    if flip_h:
-        arr_copy = np.flip(arr_copy, 0)
-    return arr_copy
+    # print(corner_tiles)
+    yield prod(corner_tiles)
+    # input()
 
+    print(edges_with_connections)
+    input()
 
-def get_mutated_tile(tile, rotation, flip_v, flip_h):
-    # @memo
-    return apply_orientation(tiles[tile], rotation, flip_v, flip_h)
+    unfound_tiles = set(data.keys())
+    unfound_edges = {k for k, v in edges_to_ids.items()}
+    first_corner = corner_tiles.pop()
 
+    puzzle_grid = {(0, 0): {'tile_id': first_corner, 'grid': data[first_corner], 'unfound': edges_to_ids[first_corner]}}
+    unfound_tiles.remove(first_corner)
 
-def find_match(tile, side, orientation):
-    target = get_mutated_tile(tile, *orientation)
-    opposite_side = (side + 2) % 4
-    matches = []
-    side_to_match = get_edge(target, side)
-    # print(side_to_match, 'was found this many times:', all_edges_ct[side_to_match])
-    for e in all_tiles:
-        if side_to_match not in all_tile_edges[e]:  # MASSIVE speedup
-            continue
-        for rotations in range(0, 2):
-            for flipv in range(0, 2):
-                for fliph in range(0, 2):
-                    if tile == e:
-                        continue
-                    candidate = get_mutated_tile(e, rotations, flipv, fliph)
-                    if side_to_match == get_edge(candidate, opposite_side):
-                        return e, (rotations, flipv, fliph)
+    for grid_id, grid in data.items():
+        all_rots = []
+        r = grid
+        for _ in range(4):
+            r = rotate_2d(r, 90, in_place=True, width=9, height=9)
+            print(r)
+            all_rots.append(r)
+            all_rots.append({(k[1], k[0]): v for k, v in r.items()})
+        data[grid_id] = all_rots
 
+    dir_map = {0: (0, -1), 1: (1, 0), 2: (0, 1), 3: (-1, 0)}
 
-corners = [(1, 1), (-1, 1), (1, -1), (-1, -1)]  # oops
-dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-
-def get_adj_unfilled(x, y):
-    adjs = []
-    for m_x, m_y in dirs:
-        adj_x, adj_y = x + m_x, y + m_y
-        try:
-            adj = tile_map[adj_x, adj_y]
-            if adj_x < 0 or adj_y < 0:
+    # edges_to_ids
+    # ids_to_edges
+    def find_next_tile(cur):
+        current_data = puzzle_grid[cur]
+        current_grid = current_data['grid']
+        current_id = current_data['tile_id']
+        current_edges = current_data['unfound']
+        for edge in current_edges:
+            adjacent_tile_id = [n for n in edges_to_ids[edge] if n != current_id]
+            if not adjacent_tile_id or adjacent_tile_id[0] not in unfound_tiles:
                 continue
-            # print('got', adj_x, adj_y)
-            if adj == -1:
-                adjs.append((adj_x, adj_y))
-        except IndexError:
-            continue
-    return adjs
+            adjacent_tile_id = adjacent_tile_id[0]
+            print(adjacent_tile_id)
+            for current_edge_index, current_edge_slice in enumerate(edge_steps):
+                opposite_slice = current_edge_index - 2
+                current_slice = grid_slice(current_grid, *current_edge_slice)
+                print('comparing')
+                print_2d(' ', current_grid)
+                print('to')
+                for adjacent_tile_grid in data[adjacent_tile_id]:
+                    adjacent_slice = grid_slice(adjacent_tile_grid, *edge_steps[opposite_slice])
+                    if adjacent_slice == current_slice:
+                        print('found match!', adjacent_slice, current_slice, )
+                        next_loc = vadd(cur, dir_map[current_edge_index])
+                        puzzle_grid[next_loc] = {'tile_id': adjacent_tile_id, 'grid': adjacent_tile_grid}
+                        unfound_tiles.remove(adjacent_tile_id)
+                        return next_loc
 
+                    print_2d(' ', adjacent_tile_grid)
+            input()
 
-def start_filling_at(x, y):
-    # print('starting to fill at ', x, y)
-    unfilled = get_adj_unfilled(x, y)
-    # print('unfilled:', unfilled)
-    if len(unfilled) == 0:
-        return
-    # random.shuffle(unfilled)  # makes it more interesting
-    for coords_to_fill in unfilled:
-        if tile_map[coords_to_fill] != -1:
-            return
-        # print('offset:', (coords_to_fill[0] - x, coords_to_fill[1] - y))
-        direction = dirs.index((coords_to_fill[0] - x, coords_to_fill[1] - y))
-        # print('calced direction', coords_to_fill[0], x, coords_to_fill[1], y, '=', direction)
-        found_id, found_orientation = find_match(tile_map[x, y], direction, orientations[x, y])
+    cur = (0, 0)
+    while unfound_tiles:
+        print('cur', cur)
+        print('a', unfound_tiles)
+        print('b', puzzle_grid.keys())
+        cur = find_next_tile(cur)
+        if not cur:
+            cur = random.choice(tuple(puzzle_grid.keys()))
 
-        tile_map[coords_to_fill] = found_id
-        # print('coords:', coords_to_fill)
-        # print(found_id, found_orientation, orientations[coords_to_fill])
-        orientations[coords_to_fill] = found_orientation
+            # print(current_id, edge,
 
-        # print(get_mutated_tile(tile_map[x, y], *orientations[x, y]), '\n MATCHES on side', direction, '\n',
-        #       get_mutated_tile(found_id, *found_orientation))
+        # for found_tile_id, found_tile_data in puzzle_grid.items():
+        #     uf = found_tile_data['unfound_edges']
+        #     if not uf:
+        #         continue
+        #     print(found_tile_id, found_tile_data)
+        #
+        #     for unfound_edge in uf:
+        #         matching_tiles = edges_to_ids[unfound_edge]
+        #         if len(matching_tiles) == 1:
+        #             del edges_to_ids[unfound_edge]
+        #             found_tile_data['unfound_edges'].remove(unfound_edge)
+        #             print('aaa', ids_to_edges[found_tile_id])
+        #         print(matching_tiles)
+        #     input()
 
-        print(tile_map)
-        start_filling_at(*coords_to_fill)
+    print(puzzle_grid)
 
+    # print(puzzle_grid)
+    # print(ids_to_edges)
+    # print(edges_with_connections)
 
-def render_array(bd):
-    # as_str = np.array2string(np.swapaxes(bd, 0, 1), max_line_width=200, separator=' ', threshold=1000, edgeitems=1000,
-    as_str = np.array2string(bd, max_line_width=220, separator=' ', threshold=1000, edgeitems=1000,
-                             formatter={'str_kind': lambda x: x})
-    # print('\x1b[1;1H\r', re.sub('[\[\]]', '', as_str), end='\n')
-    print('', re.sub('[\[\]]', '', as_str), end='\n')
+    # while edges_to_ids:
+    #     pass
 
-
-def search_for_sea_monsters_at(to_search, x, y):
-    try:
-        for y2, x2 in sea_set:
-            # print('looking in ', x+x2, y+y2)
-            point = to_search[x + x2, y + y2]
-            if point != '#':
-                # print(point)
-                return False
-        for y2, x2 in sea_set:
-            to_search[x + x2, y + y2] = 'O'
-        return True
-    except IndexError:
-        return False
-
-
-def search_for_sea_monsters():
-    monster_ct = 0
-    for rotations in range(0, 2):
-        for flipv in range(0, 2):
-            for fliph in range(0, 2):
-                to_search = apply_orientation(full_map, rotations, flipv, fliph)
-                for map_x, map_y in np.ndindex(full_map.shape):
-                    res = search_for_sea_monsters_at(to_search, map_x, map_y)
-                    # print(map_x,map_y,res, rotations, flipv, fliph)
-                    if res:
-                        monster_ct += 1
-                if monster_ct > 0:
-                    # print(rotations, flipv, fliph, monster_ct)
-                    # render_array(to_search)
-                    pass
-    return monster_ct
+    yield None
 
 
 def p2():
     return None
 
 
-f = 'd20.txt'
-if len(sys.argv) > 1:
-    f = sys.argv[1]
+setday(20)
 
-with open(f) as file:
-    data = [[x for x in tile.split('\n')] for tile in file.read().strip('\n').split('\n\n')]
+# data = parselines(get_ints)
+# grid, inverse, unique = parsegrid()
 
-tiles = {}
-for tile in data:
-    key = [int(i) for i in tile.pop(0).replace(':', '').split(' ') if i.isdigit()][0]
-    tile = [[1 if y == '#' else 0 for y in x] for x in tile]
-    tiles[key] = np.array(tile, dtype=np.uint8)
+with open_default() as file:
+    data_raw = [chunk.strip().split('\n') for chunk in file.read().strip().split('\n\n')]
+data = defaultdict(dict)
+for tile in data_raw:
+    n = get_ints(tile[0])[0]
+    for y, line in enumerate(tile[1:]):
+        for x, c in enumerate(line):
+            data[n][(x, y)] = c
 
-all_tile_edges = {}
-all_edges_ct = defaultdict(int)
-for k, v in tiles.items():
-    edges = set()
-    all_tile_edges[k] = edges
+# print(data)
 
-    for d in range(4):
-        edges.add(get_edge(v, d, True))
-        edges.add(get_edge(v, d, False))
+verbose = '-v' in sys.argv or '--verbose' in sys.argv
 
-    for edge in edges:
-        all_edges_ct[edge] += 1
+gen = solve()
+print('part1:', next(gen))  # 00:35:47
+print('part2:', next(gen))
 
-    pass
-# print(all_edges_ct)
-
-corner_tiles = set()
-all_tiles = set()
-ret = 1
-for tile, edges in all_tile_edges.items():
-    uniques = 0
-    for edge in edges:
-        if all_edges_ct[edge] == 1:
-            uniques += 1
-    if uniques == 4:
-        corner_tiles.add(tile)
-        ret *= tile
-    all_tiles.add(tile)
-
-print(f'part1: {ret}')
-
-edge_len = int(math.sqrt(len(tiles)))
-tile_size = 10
-
-# map of keys
-tile_map = np.ndarray((edge_len, edge_len), dtype=int)
-
-orientations = np.ndarray((edge_len, edge_len), dtype=tuple)
-
-tile_map.fill(-1)
-orientations.fill(tuple())
-# print(tile_map)
-
-
-tile_map[0, 0] = corner_tiles.pop()
-top_left = [get_edge(tiles[tile_map[0, 0]], i, False) for i in range(0, 4)]
-tl_rot = [all_edges_ct[e] for e in top_left]
-rot = 0
-while tl_rot != [1, 2, 2, 1]:
-    tl_rot = tl_rot[1:] + tl_rot[:1]
-    rot += 1
-# print(top_left, tl_rot, rot)
-orientations[0, 0] = (rot, 0, 0)
-
-all_tiles |= corner_tiles
-
-start_filling_at(0, 0)
-
-# print(tile_map)
-# print(orientations)
-
-inset_size = tile_size - 2
-
-full_map = np.full((edge_len * inset_size, edge_len * inset_size), dtype=str, fill_value='.')
-
-sea_monster = '''
-                  # 
-#    ##    ##    ###
- #  #  #  #  #  #   
-'''.strip('\n').split('\n')
-sea_monster_arr = [[c for c in line] for line in sea_monster]
-sea_set = set()
-for x, line in enumerate(sea_monster):
-    for y, c in enumerate(line):
-        if c == '#':
-            sea_set.add((y, x))
-# print(sea_set)
-# print('SsSsSsS')
-# for line in sea_monster:
-#     print(line)
-# print('SsSsSsS\n', sea_set)
-
-for ix, iy in np.ndindex(tile_map.shape):
-    # full_map = np.full((edge_len * inset_size, edge_len * inset_size), dtype=str, fill_value='.')
-    # print(ix, iy, tile_map[ix, iy])
-    to_copy = get_mutated_tile(tile_map[ix, iy], *orientations[ix, iy])
-    to_copy = apply_orientation(to_copy, 1, 0, 1)
-    # print(to_copy)
-    for y in range(1, 9):
-        for x in range(1, 9):
-            full_map[ix * inset_size + x - 1, iy * inset_size + y - 1] = '#' if to_copy[x, y] == 1 else '.'
-    # render_array(full_map)
-
-ct = search_for_sea_monsters()
-# print(ct, len(sea_set))
-# print((full_map == "#"), (full_map == "#").sum())
-
-print(f'part1: {ret}')
-print(f'part2: {(full_map == "#").sum() - (ct * len(sea_set))}')
-
-# render_array(full_map)
-# print('\n\nRotated 90 deg:')
-# render_array( apply_orientation(full_map, 1, 0, 1) )
+# print('part1: %d\npart2: %d' % solve())
--- EC2025/q11.py
+++ EC2025/q11.py
@@ -3,7 +3,7 @@ from ec import *
 
 def p1():
     data = [x[0] for x in parse_lines(1, get_ints)]
-    print(data, sum(data))
+
     round_counter = 0
     moved = True
     while moved:
@@ -17,9 +17,6 @@ def p1():
             round_counter += 1
             if round_counter == 10:
                 return sum((i + 1) * n for i, n in enumerate(data))
-
-
-    print(data, sum(data), round_counter)
 
     moved = True
     while moved:
@@ -35,18 +32,13 @@ def p1():
             if round_counter == 10:
                 return sum((i + 1) * n for i, n in enumerate(data))
 
-
-    print(data, sum(data), round_counter)
-    # data = parse_lines(1, get_ints)
-    # data = parse_double_break(1)
-
     return
 
 
 def p2():
     data = [x[0] for x in parse_lines(2, get_ints)]
+    print(data)
 
-    print(data, sum(data))
     round_counter = 0
     moved = True
     while moved:
@@ -58,6 +50,9 @@ def p2():
                 moved = True
         if moved:
             round_counter += 1
+
+    print(data, round_counter)
+    return
 
     moved = True
     while moved:
@@ -76,6 +71,82 @@ def p2():
     # data = parse_double_break(1)
 
     return round_counter
+
+
+def p2_fast():
+    data = [x[0] for x in parse_lines(4, get_ints)]
+    data_original = data.copy()
+
+    round_counter = 0
+
+    did_work = True
+    while did_work:
+        did_work = False
+        for i in range(len(data)-1):
+            j = 1
+            # print('checking', i, data[i])
+            while (i+j) < len(data) and data[i + j - 1] > data[i + j]:
+                # print(i, j, data[i], data[j])
+                j += 1
+            if j == 1:
+                continue
+            did_work = True
+            # print('range', data[i:i+j])
+            sum_of_range = sum(data[i:i+j])
+            # print('sum:', sum_of_range)
+            d, m = sum_of_range//j, sum_of_range%j
+            # print(f'd,m {d,m}')
+            round_counter += max(data[i:i+j]) - min(data[i:i+j])
+            for k in range(i, i+j):
+                # print('changed', data[k], 'to', d)
+                data[k] = d
+            for k in range(i+j-m, i+j):
+                data[k] += 1
+                # print('added', 1, 'to', data[k], 'at index', k)
+
+        # input()
+    print(data, data_original)
+
+    # round_counter = sum(b-a for a,b in zip(data, data_original) if b-a > 0)
+
+    return round_counter
+
+
+
+def p2_faster():
+    data = [x[0] for x in parse_lines(4, get_ints)] # just the input parsed into a list
+
+    round_counter = 0
+    did_work = True
+
+    while did_work:
+        last_state = data.copy()
+        did_work = False
+        for i in range(len(data)-1):
+            j = 1
+            while (i+j) < len(data) and data[i + j - 1] > data[i + j]:
+                j += 1
+            if j == 1:
+                continue
+            did_work = True
+
+            sum_of_range = sum(data[i:i+j])
+
+            q, m = sum_of_range // j, sum_of_range % j
+
+            this_change = max(data[i:i + j]) - min(data[i:i + j])
+            # print(this_change)
+
+            for k in range(i, i+j):
+                data[k] = q
+            for k in range(i+j-m, i+j):
+                data[k] += 1
+        diffs = [a-b for a,b in zip(data, last_state)]
+        change = sum(i for i in diffs if i > 0)
+        print(diffs, change)
+        round_counter += change
+
+    print(data, round_counter)
 
 
 def p3():
@@ -106,6 +177,10 @@ def p3():
 
 setquest(11)
 
-print('part1:', p1())
-print('part2:', p2())
-print('part3:', p3())
+# 2795100
+# 7696326
+
+# print('part1:', p1())
+# print('part2:', p2())
+print('part2:', p2_faster())
+# print('part3:', p3())
