c8144c05743b86d84c61ae575cd097ac825953e4
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-02 00:39:19 -0500

    Wash 2025 day 2
    Pretty sure there's some clever solution here that avoids having to check every number in the given ranges, but I can't think of it and don't care to try harder.

--- AoC2025/d2.py
+++ AoC2025/d2.py
@@ -1,81 +1,44 @@
-import sys
-import re
-# import numpy as np
-from collections import defaultdict  # defaultdict(int)
-import functools  # @functools.cache
-from collections import *
-from math import *
-from pprint import pprint
-
 from aoc import *
 
 
-# print_2d(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# print_2d_repl(padding, *dicts, constrain=(-256, -256, 256, 256)):
-# INF = sys.maxsize
-# @memo
-# splat(f)
-# bfs(src, tgt, {n1: [n2, n3, n4]})
-# wbfs(src, tgt, edge_func)
-# astar(src, tgt, edge_func, heur_func)
-# ccs(srcs, neigh_func, filt_func?)
-# Dedup().seen(x)
+def is_invalid(i):
+    i_str = str(i)
+    i_str_len = len(i_str)
+    for divisor in range(2, i_str_len + 1):
+        if i_str_len % divisor != 0:
+            continue
+
+        interval = len(i_str) // divisor
+        base = i_str[:interval]
+        for offset in range(interval, i_str_len, interval):
+            if i_str[offset:offset + interval] != base:
+                break
+        else:
+            return True
+    return False
 
 
-def p1():
-    acc = 0
-    for i_range in data.split(','):
-        a, b = (int(i) for i in i_range.split('-'))
-        for i in range(a, b + 1):
-            i_s = str(i)
-            m = len(i_s) // 2
-            if i_s[:m] == i_s[m:]:
-                acc += i
-    return acc
+def solve():
+    acc_p1 = 0
+    acc_p2 = 0
+    for range_str in data.split(','):
+        l, r = (int(i) for i in range_str.split('-'))
+        for i in range(l, r + 1):
+            # part 1
+            i_str = str(i)
+            midpoint = len(i_str) // 2
+            if i_str[:midpoint] == i_str[midpoint:]:
+                acc_p1 += i
 
-
-def p2():
-    def is_invalid(i):
-        i_s = str(i)
-        ln = len(i_s)
-        for d in range(2, ln + 1):
-            # print(d)
-            if ln % d != 0:
-                continue
-
-            interval = len(i_s) // d
-            base = i_s[:interval]
-            # print(f'{i_s}, ln={ln}, base={base}, interval={interval}, divisor={d}')
-            for offset in range(interval, ln, interval):
-                # print(offset, i_s[offset:offset + interval], '==', base)
-                # input()
-                if i_s[offset:offset + interval] != base:
-                    break
-            else:
-                # print('passed = invalid')
-                return True
-        return False
-
-    acc = 0
-    for i_range in data.split(','):
-        a, b = (int(i) for i in i_range.split('-'))
-        for i in range(a, b + 1):
+            # part 2
             if is_invalid(i):
-                acc += i
-    return acc
+                acc_p2 += i
+    return acc_p1, acc_p2
 
 
 if __name__ == '__main__':
     setday(2)
 
     data = parselines()[0]
-    # data = parselines(get_ints)
-    # grid, inverse, unique = parsegrid()
 
-    # with open_default() as file:
-    #     data = get_ints(file.read())
-
-    print('part1:', p1())
-    print('part2:', p2())
-
-    # print('part1: %d\npart2: %d' % solve())
+    print('part1: %d\npart2: %d' % solve())
