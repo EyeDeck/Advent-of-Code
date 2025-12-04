928d668d9373f0ca705a4a5dcbaadf0603a6992f
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-04 01:06:54 -0500

    Wash 2025 day 4
    Code golfed it a little.

--- AoC2025/d4.py
+++ AoC2025/d4.py
@@ -1,48 +1,22 @@
-
 from aoc import *
 
 
 def p1():
-    acc = 0
-
-    rolls = set(inverse['@'])
-    for point in rolls:
-        ct = 0
-        for d in DIAGDIRS:
-            o = vadd(d, point)
-            if o in rolls:
-                ct += 1
-        if ct < 4:
-            acc += 1
-
-    return acc
+    return sum(1 for p in rolls if sum(1 for d in DIAGDIRS if vadd(d, p) in rolls) < 4)
 
 
 def p2():
-    rolls = set(inverse['@'])
-    start = len(rolls)
-    last = 0
-    while True:
-        to_remove = set()
-        for point in rolls:
-            ct = 0
-            for d in DIAGDIRS:
-                o = vadd(d, point)
-                if o in rolls:
-                    ct += 1
-            if ct < 4:
-                to_remove.add(point)
-        rolls -= to_remove
-        last_temp = last
-        last = len(rolls)
-        if last_temp == last:
-            return start - last
+    r = rolls.copy()
+    while (n := {p for p in r if sum(vadd(d, p) in r for d in DIAGDIRS) >= 4}) != r:
+        r = n
+    return len(rolls) - len(r)
 
 
 if __name__ == '__main__':
     setday(4)
 
     grid, inverse, unique = parsegrid()
+    rolls = set(inverse['@'])
 
-    print('part1:', p1() )
-    print('part2:', p2() )
+    print('part1:', p1())
+    print('part2:', p2())
