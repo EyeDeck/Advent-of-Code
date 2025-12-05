3147de56876add4a3919a4c692302bc7d8a0d91c
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-05 00:33:23 -0500

    Solve 2025 day 5
    Day   -Part 1-   -Part 2-
      5   00:12:11   00:14:34
    
    I literally just committed the solution for part 2 to my aoc library a week ago. It came up during Everybody Codes, although I can't recall which day offhand, and I recalled having to write something similar for an older AoC, so I cleaned it up and put it in my library for the future.

---
+++ AoC2025/d5.py
@@ -0,0 +1,37 @@
+from aoc import *
+
+
+def p1():
+    def is_in_range(id):
+        for l, r in ranges:
+            if l <= id <= r:
+                return True
+        return False
+
+    acc = 0
+    for id in available:
+        if is_in_range(id):
+            acc += 1
+    return acc
+
+
+def p2():
+    merged_ranges = merge_ranges(ranges)
+    acc = 0
+    for l, r in merged_ranges:
+        acc += r-l + 1
+    return acc
+
+
+if __name__ == '__main__':
+    setday(5)
+
+    with open_default() as file:
+        data = file.read()
+
+    ranges_raw, available_raw = data.split('\n\n')
+    ranges = [tuple(int(s) for s in line.split('-')) for line in ranges_raw.split('\n')]
+    available = [int(line) for line in available_raw.split('\n')]
+
+    print('part1:', p1() )
+    print('part2:', p2() )
