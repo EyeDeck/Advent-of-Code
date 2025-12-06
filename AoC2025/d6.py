08fcac861a0b07a0fde111b8b97321a4ae0e33e5
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-06 00:35:46 -0500

    Wash 2025 day 6
    Meh, no major algorithmic improvements possible here.

--- AoC2025/d6.py
+++ AoC2025/d6.py
@@ -20,12 +20,7 @@ def p1():
 
 def p2():
     grid, inverse, unique = parsegrid()
-
     bounds = grid_bounds(grid)
-
-    print_2d(' ', grid)
-
-    print(bounds)
 
     acc = 0
 
@@ -34,33 +29,27 @@ def p2():
     for x in range(bounds[2], -1, -1):
         digits = []
         clear_flag = False
-        for y in range(bounds[3]+1):
-            pos = (x,y)
+        for y in range(bounds[3] + 1):
+            pos = (x, y)
             if pos not in grid:
                 continue
             c = grid[pos]
-            print('c:', c)
             if c.isnumeric():
                 digits.append(c)
-            if c in {'+', '*'}:
+            elif c in {'+', '*'}:
                 if c == '+':
                     op = sum
                 else:
                     op = math.prod
                 clear_flag = True
-            print(digits)
 
         if digits:
             numbers.append(int(''.join(digits)))
             digits.clear()
 
-        print(numbers)
-
         if clear_flag:
             acc += op(numbers)
             numbers.clear()
-
-        print(x)
 
     return acc
 
@@ -68,5 +57,5 @@ def p2():
 if __name__ == '__main__':
     setday(6)
 
-    print('part1:', p1() )
-    print('part2:', p2() )
+    print('part1:', p1())
+    print('part2:', p2())
