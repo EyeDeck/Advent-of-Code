b23177a3a1f1b02fecf38d8ea8605de835792dc7
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-08 01:21:58 -0500

    Fix 2025 day 8
    - Fixed an off-by-1 I introduced while washing
    - Switched to the standard library math.dist() function
    - Added a -p [int] command-line parameter (default 1000) so the example can be tested without having to edit the script, e.g.:
    python d8.py d8.txt
    python d8.py d8-example.txt -p 10

--- AoC2025/d8.py
+++ AoC2025/d8.py
@@ -4,24 +4,23 @@ from aoc import *
 
 
 def solve():
-    def get_dist(a, b):
-        return sum(abs(i) ** 2 for i in vsub(a, b))
-
     dists = []
     for i, a in enumerate(data):
         for j, b in enumerate(data[i + 1:]):
-            dists.append((get_dist(a, b), a, b))
+            dists.append((math.dist(a, b), a, b))
     dists.sort()
 
     G = nx.Graph()
     G.add_nodes_from(data)
     for i, connection in enumerate(dists):
-        dist, a, b = connection
-        G.add_edge(a, b)
-        if i == 1000:
+        if i == p1_pairs:  # part 1
             s_c = sorted(nx.connected_components(G), key=len, reverse=True)
             yield math.prod(len(x) for x in s_c[:3])
-        if nx.is_connected(G):
+
+        dist, a, b = connection
+        G.add_edge(a, b)
+
+        if nx.is_connected(G):  # part 2
             yield a[0] * b[0]
 
 
@@ -29,6 +28,7 @@ if __name__ == '__main__':
     setday(8)
 
     data = parselines(lambda x: tuple(get_ints(x)))
+    p1_pairs = 1000 if '-p' not in sys.argv else int(sys.argv[sys.argv.index('-p') + 1])
 
     solver = solve()
     print('part1:', next(solver))
