3ab3f2576d787547f8ed333670e2ee3ebb1fc007
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2025-12-09 04:45:55 -0500

    Wash 2025 day 9
    An anon on /g/ mentioned 'coordinate compression', so I googled it to find out what it was, then made a rough guess regarding how it might be applied.
    Amazingly it worked first try. New runtime is ~1-2 seconds instead of ~15 minutes. Incredible.
    I was going to rewrite it to use ranges, but this might be faster (and waaaay less work).

--- AoC2025/d9.py
+++ AoC2025/d9.py
@@ -12,17 +12,27 @@ def p1():
 
 
 def p2():
-    bounds = set()
-    for i in range(len(data) - 1, -1, -1):
-        a, b = data[i], data[i - 1]
-        bounds |= set(p for p in bresenham(a, b))
-    # print(bounds)
-    # print_2d(' ', {k:'#' for k in bounds})
-    # print({(x//20 for x in k):'#' for k in bounds})
-    bottom_right = max(data, key=lambda x: x[0] * x[1])
-    search_line = bresenham((0, 0), bottom_right)
+    x_to_cmp = {k: v for v, k in enumerate(sorted({c[0] for c in data}))}
+    y_to_cmp = {k: v for v, k in enumerate(sorted({c[1] for c in data}))}
 
-    SCALE = 1
+    def compress(c):
+        return x_to_cmp[c[0]], y_to_cmp[c[1]]
+
+    cmp_to_x = {k: v for v, k in x_to_cmp.items()}
+    cmp_to_y = {k: v for v, k in y_to_cmp.items()}
+
+    def decompress(c):
+        return cmp_to_x[c[0]], cmp_to_y[c[1]]
+
+    data_cmp = [compress(c) for c in data]
+
+    bounds = set()
+    for i in range(len(data_cmp) - 1, -1, -1):
+        a, b = data_cmp[i], data_cmp[i - 1]
+        bounds |= set(p for p in bresenham(a, b))
+
+    bottom_right = max(data_cmp, key=lambda x: x[0] * x[1])
+    search_line = bresenham((-1, -1), bottom_right)
 
     outside_point = None
     for i in range(len(search_line)):
@@ -34,51 +44,48 @@ def p2():
     for point in bounds:
         expanded |= {vadd(o, point) for o in DIAGDIRS}
 
-
-
-    outline = {outside_point}
+    forbidden_zone = {outside_point}
     frontier = {outside_point}
     while frontier:
         point = frontier.pop()
         n = [vadd(o, point) for o in DIAGDIRS]
-        n = {p for p in n if p in expanded and p not in outline and p not in bounds}
+        n = {p for p in n if p in expanded and p not in forbidden_zone and p not in bounds}
         frontier |= n
-        outline |= n
+        forbidden_zone |= n
 
-    print_2d(
-        '  ',
-        {tuple(x // SCALE for x in k): '.' for k in expanded},
-        {tuple(x // SCALE for x in k): '+' for k in outline},
-        {tuple(x // SCALE for x in k): '#' for k in bounds},
-        # {tuple(x // SCALE for x in k): '~' for k in search_line},
-        {tuple(x // SCALE for x in outside_point): '@'}
-    )
+    scale = 1 if '-s' not in sys.argv else int(sys.argv[sys.argv.index('-s') + 1])
+
+    if verbose:
+        print_2d(
+            '  ',
+            {tuple(x // scale for x in k): '.' for k in expanded},
+            {tuple(x // scale for x in k): '+' for k in forbidden_zone},
+            {tuple(x // scale for x in k): '#' for k in bounds},
+            # {tuple(x // scale for x in k): '~' for k in search_line},
+            {tuple(x // scale for x in outside_point): '@'}
+        )
 
     def get_inside(a, b):
-        #  x1 y1 -> x2 y1 -> x2 y2 -> x1 y2
+        #  x1 y1 -> x2 y1 -> x2 y2 -> x1 y2 -> x1 y1
         corners = [a, (b[0], a[1]), b, (a[0], b[1])]
-        # print(corners)
-        # lines = set()
         for i in range(3, -1, -1):
             line = set(x for x in bresenham(corners[i], corners[i - 1]))
-            if not line.isdisjoint(outline):
+            if not line.isdisjoint(forbidden_zone):
                 return False
-            # lines |= line
-            # print('\t', line)
-        # print_2d(' ', {tuple(x // SCALE for x in k): '#' for k in lines})
         return True
 
     best = 0
-    for i, a in enumerate(data):
-        for j, b in enumerate(data[i + 1:]):
+    for i, a in enumerate(data_cmp):
+        if verbose:
             print(i, best, end='\r')
+        for j, b in enumerate(data_cmp[i + 1:]):
             if not get_inside(a, b):
                 continue
-            w = abs(a[0] - b[0]) + 1
-            h = abs(a[1] - b[1]) + 1
+            a_real, b_real = decompress(a), decompress(b)
+            w = abs(a_real[0] - b_real[0]) + 1
+            h = abs(a_real[1] - b_real[1]) + 1
             best = max(w * h, best)
 
-    # print(len(bounds))
     return best
 
 
